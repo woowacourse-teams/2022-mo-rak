f50580ac31e8c463284ba9fffb86421db52093f9
cjlee38 <cjlee38@github.com>
2022-07-03 14:49:03 +0900

    chore: 프로젝트 셋업

---
+++ src/main/java/com/morak/back/MorakBackApplication.java
@@ -0,0 +1,13 @@
+package com.morak.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorakBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorakBackApplication.class, args);
+    }
+
+}
