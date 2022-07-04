5f797032cb681dc3a83f3f62b2ad340eb42628c3
sangminsong <ssmin940606@gmail.com>
2022-07-04 17:37:41 +0900

    chore: 초기 개발 환경 설정
    Co-authored-by: winnie_yeji <rladpwl0512@users.noreply.github.com>

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 3000
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: 'ts-loader',
+        exclude: ['/node_modules/']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset'
+      },
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '...']
+  }
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+
+  return config;
+};
