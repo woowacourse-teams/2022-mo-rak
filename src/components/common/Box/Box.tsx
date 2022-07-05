5dca34508f3e6d1122ced7868722017732275ec0
sangminsong <ssmin940606@gmail.com>
2022-07-05 15:57:11 +0900

    feat: box 컴포넌트 생성
    Co-authored-by: winnie_yeji <rladpwl0512@users.noreply.github.com>

---
+++ src/components/common/Box/Box.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+import { CSSObject } from '@emotion/react';
+
+type Props = {
+  children: React.ReactNode;
+} & Pick<CSSObject, 'width' | 'height' | 'borderRadius'>;
+
+function Box({ children, width, height, borderRadius = '15px' }: Props) {
+  return (
+    <Wrapper width={width} height={height} borderRadius={borderRadius}>
+      {children}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div<
+  Pick<CSSObject, 'width' | 'height' | 'borderRadius'>
+>(
+  ({ theme, width, height, borderRadius }) => `
+    width: ${width};
+    height: ${height};
+    border-radius: ${borderRadius};
+    box-shadow: 0px 4px 4px ${theme.colors.TRANSPARENT_BLACK_25};
+    background-color: ${theme.colors.WHITE_100};
+  `
+);
+
+export default Box;
