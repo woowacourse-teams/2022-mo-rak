8a4b1981cc86ad1b329d788ae70f5614895a51ea
cjlee38 <cjlee38@github.com>
2022-07-07 16:04:08 +0900

    feat(poll): 커스텀 예외 및 처리 핸들러 추가

---
+++ src/main/java/com/morak/back/poll/ui/dto/ExceptionResponse.java
@@ -0,0 +1,10 @@
+package com.morak.back.poll.ui.dto;
+
+public class ExceptionResponse {
+
+    private final String message;
+
+    public ExceptionResponse(String message) {
+        this.message = message;
+    }
+}
