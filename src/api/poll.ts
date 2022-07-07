b575dd100496ea9047aa33a30b8a36585ec04d73
sangminsong <ssmin940606@gmail.com>
2022-07-07 22:38:10 +0900

    feat(poll): 투표 관련 API 요청 보내는 여러 함수 구현
    Co-authored-by: winnie_yeji <rladpwl0512@users.noreply.github.com>

--- src/api/poll.ts
+++ src/api/poll.ts
@@ -1,7 +1,15 @@
-import { PollDataInterface } from '../components/PollCreateForm/PollCreateForm';
+import { PollInterface, PollCreateType } from '../types/poll';
 import fetcher from '../utils/fetcher';
 
-const createPoll = (pollData: PollDataInterface) =>
-  fetcher({ method: 'POST', path: 'polls', body: pollData });
+// TODO: getPollInfo => info suffix에 대해 생각해보기
+const getPollInfo = (id: PollInterface['id']) => fetcher({ method: 'GET', path: `polls/${id}` });
+const getPollItems = (id: PollInterface['id']) =>
+  fetcher({ method: 'GET', path: `polls/${id}/items` });
+const getPollResult = (id: PollInterface['id']) =>
+  fetcher({ method: 'GET', path: `polls/${id}/result` });
+const createPoll = (poll: PollCreateType) => fetcher({ method: 'POST', path: 'polls', body: poll });
+// TODO: 백엔드 API 이상으로 인해 itemIds 임시 타입 적용
+const progressPoll = (id: PollInterface['id'], itemIds: string[]) =>
+  fetcher({ method: 'PUT', path: `polls/${id}`, body: itemIds });
 
-export { createPoll };
+export { createPoll, getPollInfo, getPollItems, getPollResult, progressPoll };
