eac5552b372fa28f693775150be9bd0a0f8f2335
sangminsong <ssmin940606@gmail.com>
2022-07-13 17:16:01 +0900

    test: Radio 컴포넌트 스토리 생성

---
+++ frontend/src/components/common/Radio/Radio.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import Radio from './Radio';
+
+export default {
+  title: 'Reusable Components/Radio',
+  component: Radio
+} as Meta;
+
+const Template: Story = (args) => <Radio name="story" {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
