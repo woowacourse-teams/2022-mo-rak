14a5950190a11d466182f6380c057586d31a3edf
sangminsong <ssmin940606@gmail.com>
2022-07-13 17:14:40 +0900

    feat: Radio 컴포넌트 생성

--- frontend/src/components/common/Radio/Radio.tsx
+++ frontend/src/components/common/Radio/Radio.tsx
@@ -1,5 +1,30 @@
-// import React, { InputHTMLAttributes } from 'react';
+import styled from '@emotion/styled';
+import React, { InputHTMLAttributes, PropsWithChildren } from 'react';
+import FlexContainer from '../FlexContainer/FlexContainer';
 
-// function Radio() {}
+interface Props extends PropsWithChildren<InputHTMLAttributes<HTMLInputElement>> {
+  name: string;
+}
 
-// export default Radio;
+function Radio({ id, name, children }: Props) {
+  return (
+    <FlexContainer alignItems="center">
+      <StyledInput type="radio" id={id} name={name} />
+      <StyledLabel htmlFor={id}>{children}</StyledLabel>
+    </FlexContainer>
+  );
+}
+
+const StyledInput = styled.input`
+  cursor: pointer;
+`;
+
+const StyledLabel = styled.label`
+  cursor: pointer;
+  width: 100%;
+  height: 100%;
+  font-size: 1.6rem;
+  text-align: center;
+`;
+
+export default Radio;
