0db737dc0520ac9755e2053becafc4d7a520aebd
sangminsong <ssmin940606@gmail.com>
2022-07-13 16:15:57 +0900

    feat: storybook 타입 추가

--- frontend/src/components/common/Input/Input.stories.tsx
+++ frontend/src/components/common/Input/Input.stories.tsx
@@ -1,40 +1,19 @@
 import React from 'react';
+import { Meta, Story } from '@storybook/react';
 import theme from '../../../styles/theme';
+
 import Input from './Input';
 
 export default {
   title: 'Reusable Components/Input',
   component: Input
-};
+} as Meta;
 
-function OutlinedTemplate(args) {
-  return <Input {...args} />;
-}
+const Template: Story = (args) => <Input {...args} />;
 
-export const Outlined = OutlinedTemplate.bind({});
-Outlined.args = {
-  color: theme.colors.BLACK_100,
-  colorScheme: theme.colors.PURPLE_100,
-  width: '74.4rem',
-  height: '3.6rem',
-  borderRadius: '10px',
-  variant: 'outlined',
-  fontSize: '1rem',
-  placeholder: '선택항목을 입력해주세요!'
-};
-
-function UnstyledTemplate(args) {
-  return <Input {...args} />;
-}
-
-export const Unstyled = UnstyledTemplate.bind({});
-Unstyled.args = {
-  colorScheme: theme.colors.PURPLE_100,
-  width: '74.4rem',
-  height: '3.6rem',
-  variant: 'unstyled',
-  color: theme.colors.BLACK_100,
-  fontSize: '3.2rem',
-  placeholder: '투표 제목을 입력해주세요🧐',
-  textAlign: 'left'
+export const Default = Template.bind({});
+Default.args = {
+  fontSize: '5rem',
+  textAlign: 'center',
+  color: theme.colors.BLACK_100
 };
