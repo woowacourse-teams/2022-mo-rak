3720b837b6f78d86998775d4ab403b4194b74184
KIM YEJI <rladpwl0512@gmail.com>
2022-07-19 00:28:57 +0900

    design(poll): 체크박스 및 라디오 버튼 추가

--- frontend/src/components/common/Checkbox/Checkbox.tsx
+++ frontend/src/components/common/Checkbox/Checkbox.tsx
@@ -1,26 +1,45 @@
 import React, { InputHTMLAttributes, PropsWithChildren } from 'react';
 import styled from '@emotion/styled';
 import FlexContainer from '../FlexContainer/FlexContainer';
+import Check from '../../../assets/check.svg';
+import NotCheck from '../../../assets/not_check.svg';
 
 interface Props extends PropsWithChildren<InputHTMLAttributes<HTMLInputElement>> {}
 
-function Checkbox({ id, children, ...props }: Props) {
+function Checkbox({ id, children, checked, ...props }: Props) {
   return (
     <FlexContainer alignItems="center">
-      <label htmlFor={id}>
+      <StyledContainerLabel htmlFor={id}>
         <StyledInput type="checkbox" id={id} {...props} />
-        <span />
+        <StyledCheckbox src={checked ? Check : NotCheck} alt="check" />
         <StyledLabel>{children}</StyledLabel>
-      </label>
+      </StyledContainerLabel>
     </FlexContainer>
   );
 }
 
-const StyledInput = styled.input`
+const StyledContainerLabel = styled.label`
+  width: 100%;
   cursor: pointer;
 `;
+
+const StyledInput = styled.input`
+  cursor: pointer;
+  display: none;
+`;
+
+const StyledCheckbox = styled.img`
+  width: 2rem;
+  height: 2rem;
+  position: absolute;
+  left: 1.2rem;
+  top: 10px;
+`;
+
 const StyledLabel = styled.span`
   cursor: pointer;
+  font-size: 1.6rem;
+  text-align: center;
 `;
 
 export default Checkbox;
