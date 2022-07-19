61878192d423020d629d3cac55ceaaff45580c8f
al-bur <64825713+al-bur@users.noreply.github.com>
2022-07-19 19:58:52 +0900

    feat: OAuth 기능 구현 (#56)
    * feat(oauth): 랜딩 페이지 구현 및 로그인 기능 구현
    
    * feat(oauth): 랜딩페이지 접속시 로그인한 상태면 그룹 생성 및 참가 페이지로 이동하도록 기능 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import PollCreatePage from './pages/PollCreatePage/PollCreatePage';
 import PollProgressPage from './pages/PollProgressPage/PollProgressPage';
+import HomePage from './pages/HomePage/HomePage';
 import PollResultPage from './pages/PollResultPage/PollResultPage';
 import GroupInitPage from './pages/GroupInitPage/GroupInitPage';
 import FlexContainer from './components/common/FlexContainer/FlexContainer';
@@ -23,8 +24,12 @@ function App() {
               <Route path=":groupCode" element={<MainPage />} />
             </Route>
           </Route>
+          <Route path="/" element={<HomePage />} />
           <Route path="/init" element={<GroupInitPage />} />
           <Route path="/invite/:invitationCode" element={<InvitationPage />} />
+          <Route path="/create" element={<PollCreatePage />} />
+          <Route path="/progress" element={<PollProgressPage />} />
+          <Route path="/result" element={<PollResultPage />} />
           <Route path="*" element={<div>error</div>} />
         </Routes>
       </PollContext>
