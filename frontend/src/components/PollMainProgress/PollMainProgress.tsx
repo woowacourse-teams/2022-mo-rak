cd0e198417ce0121107226ebbc227537cc10b0a2
sangminsong <ssmin940606@gmail.com>
2022-07-21 22:35:33 +0900

    fix(poll): 투표 메인페이지에서 각 투표의 progress 출력 오류 해결
    - 각 투표의 progress에서의 현재 투표자수가 잘못 출력되는 문제 해결
    
    Co-authored-by: winnie_yeji <rladpwl0512@users.noreply.github.com>

--- frontend/src/components/PollMainProgress/PollMainProgress.tsx
+++ frontend/src/components/PollMainProgress/PollMainProgress.tsx
@@ -4,20 +4,27 @@ import Progress from '../common/Progress/Progress';
 import { PollInterface, PollItemResultType } from '../../types/poll';
 import FlexContainer from '../common/FlexContainer/FlexContainer';
 import { getPollResult } from '../../api/poll';
-import { GroupInterface } from '../../types/group';
+import { GroupInterface, MemberInterface } from '../../types/group';
+import { getGroupMembers } from '../../api/group';
 
 interface Props {
   pollId: PollInterface['id'];
-  groupCode?: GroupInterface['code'];
+  groupCode: GroupInterface['code'];
 }
+
+// TODO: 비효율적이다...리팩토링
+const getCurrentParticipants = (pollResult: Array<PollItemResultType>) => {
+  const allParticipants = pollResult.map((pollItemResult) => pollItemResult.members).flat();
+  const currentParticipants = allParticipants.map((participant) => participant.name);
+
+  return new Set(currentParticipants).size;
+};
 
 function PollMainProgress({ pollId, groupCode }: Props) {
   const [pollResult, setPollResult] = useState<Array<PollItemResultType>>([]);
-  const totalParticipants = pollResult.length; // TODO: 그룹 기능 추가되면, 업데이트할 것
-  const currentParticipants = pollResult.reduce(
-    (prev: number, cur: PollItemResultType) => prev + cur.count,
-    0
-  );
+  const [groupMembers, setGroupMembers] = useState<Array<MemberInterface>>([]);
+  const totalParticipants = groupMembers.length;
+  const currentParticipants = getCurrentParticipants(pollResult);
 
   useEffect(() => {
     const fetchPollResult = async (pollId: PollInterface['id']) => {
@@ -33,6 +40,25 @@ function PollMainProgress({ pollId, groupCode }: Props) {
 
     fetchPollResult(pollId);
   }, []);
+
+  useEffect(() => {
+    const fetchGroupMembers = async () => {
+      try {
+        if (groupCode) {
+          const res = await getGroupMembers(groupCode);
+
+          setGroupMembers(res);
+        }
+      } catch (err) {
+        if (err instanceof Error) {
+          // TOOD: 모든 곳에서 401 대비를 해주는 게 맞을까?
+          console.log(err);
+        }
+      }
+    };
+
+    fetchGroupMembers();
+  }, [groupCode]);
 
   return (
     <FlexContainer flexDirection="column" alignItems="end">
