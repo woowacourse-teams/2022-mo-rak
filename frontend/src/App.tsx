7a4916fb02f6d71b9557cdad71a6d69fa027071b
sangminsong <ssmin940606@gmail.com>
2022-07-21 22:31:56 +0900

    feat: 중첩 라우팅 구현
    Co-authored-by: winnie_yeji <rladpwl0512@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ import LandingPage from './pages/LandingPage/LandingPage';
 import PollResultPage from './pages/PollResultPage/PollResultPage';
 import GroupInitPage from './pages/GroupInitPage/GroupInitPage';
 import FlexContainer from './components/common/FlexContainer/FlexContainer';
-import PollContext from './contexts/PollContext';
 import SidebarLayout from './components/SidebarLayout/SidebarLayout';
 import MainPage from './pages/MainPage/MainPage';
 import InvitationPage from './pages/InvitationPage/InvitationPage';
@@ -17,25 +16,28 @@ function App() {
   return (
     // TODO: default 그룹을 찾는 요청을 해서 그룹이 있는 지 없는 지 확인하는 Route 생성?
     <FlexContainer>
-      <PollContext>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
+      <Routes>
+        <Route path="/">
+          <Route index element={<LandingPage />} />
           <Route element={<PrivateRoute />}>
             <Route element={<SidebarLayout />}>
-              <Route path="/poll" element={<PollMainPage />} />
-              <Route path="/poll/create" element={<PollCreatePage />} />
-              <Route path="/poll/:id/progress" element={<PollProgressPage />} />
-              <Route path="/poll/:id/result" element={<PollResultPage />} />
-              <Route path="/groups" element={<MainPage />}>
-                <Route path=":groupCode" element={<MainPage />} />
+              <Route path="groups/:groupCode">
+                {/* TODO: 100% groupCode가 들어오니 단언을 해줘도 좋지 않을까? */}
+                <Route index element={<MainPage />} />
+                <Route path="poll">
+                  <Route index element={<PollMainPage />} />
+                  <Route path="create" element={<PollCreatePage />} />
+                  <Route path=":pollId/progress" element={<PollProgressPage />} />
+                  <Route path=":pollId/result" element={<PollResultPage />} />
+                </Route>
               </Route>
             </Route>
-            <Route path="/init" element={<GroupInitPage />} />
+            <Route path="init" element={<GroupInitPage />} />
           </Route>
-          <Route path="/invite/:invitationCode" element={<InvitationPage />} />
+          <Route path="invite/:invitationCode" element={<InvitationPage />} />
           <Route path="*" element={<div>error</div>} />
-        </Routes>
-      </PollContext>
+        </Route>
+      </Routes>
     </FlexContainer>
   );
 }
