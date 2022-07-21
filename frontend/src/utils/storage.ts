75cd644159f5684803b3110154cd6d25d71dcf70
sangminsong <ssmin940606@gmail.com>
2022-07-21 09:47:37 +0900

    feat: storage 삭제 기능 구현

--- frontend/src/utils/storage.ts
+++ frontend/src/utils/storage.ts
@@ -10,10 +10,19 @@ const getSessionStorageItem = (key: string) => {
   return value ? JSON.parse(value) : null;
 };
 
+// TODO: <T>에 default 값을 넣어주는 건 어떨까?
 const saveLocalStorageItem = <T>(key: string, value: T | Array<T>) =>
   localStorage.setItem(key, JSON.stringify(value));
 
 const saveSessionStorageItem = <T>(key: string, value: T | Array<T>) =>
   sessionStorage.setItem(key, JSON.stringify(value));
 
-export { getLocalStorageItem, saveLocalStorageItem, getSessionStorageItem, saveSessionStorageItem };
+const removeLocalStorageItem = (key: string) => localStorage.removeItem(key);
+
+export {
+  getLocalStorageItem,
+  saveLocalStorageItem,
+  getSessionStorageItem,
+  saveSessionStorageItem,
+  removeLocalStorageItem
+};
