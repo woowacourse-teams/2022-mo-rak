3423d66668ffc9e9e086305867ab64574d36322a
Sungsan Kim <42317507+leo0842@users.noreply.github.com>
2022-07-21 15:37:26 +0900

    feat: 로그인과 투표 기능에 restdocs 구현

---
+++ backend/src/test/java/com/morak/back/poll/ui/ControllerTest.java
@@ -0,0 +1,32 @@
+package com.morak.back.poll.ui;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.morak.back.auth.application.TokenProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureRestDocs
+@MockBean(JpaMetamodelMappingContext.class)
+public class ControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected TokenProvider tokenProvider;
+
+    @BeforeEach
+    void setUp() {
+        given(tokenProvider.parsePayload(anyString())).willReturn(String.valueOf(1L));
+    }
+}
