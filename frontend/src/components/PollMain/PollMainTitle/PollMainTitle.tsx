ef74a0c130435c5d65ead54930f99fcdf09de66e
sangminsong <ssmin940606@gmail.com>
2022-07-26 21:38:57 +0900

    refactor(poll): 투표 메인페이지내 컴포넌트 props 변경
    - PollMainTitle 컴포넌트에서 title이라는 props를 받았던 것을 children으로 받도록 변경 => 일관성을 위해
    
    Co-authored-by: winnie_yeji <rladpwl0512@users.noreply.github.com>

--- frontend/src/components/PollMain/PollMainTitle/PollMainTitle.tsx
+++ frontend/src/components/PollMain/PollMainTitle/PollMainTitle.tsx
@@ -1,17 +1,9 @@
-import React, { CSSProperties } from 'react';
+import React, { PropsWithChildren } from 'react';
 
 import styled from '@emotion/styled';
-import { PollInterface } from '../../../types/poll';
 
-// TODO: interface와 type의 차이와 대부분 어떤 것을 사용하는지?
-interface Props {
-  title: PollInterface['title'];
-}
-
-// TODO: 재사용 되는데, common에 넣어야하나?...
-
-function PollMainTitle({ title }: Props) {
-  return <StyledTitle>{title}</StyledTitle>;
+function PollMainTitle({ children }: PropsWithChildren) {
+  return <StyledTitle>{children}</StyledTitle>;
 }
 
 const StyledTitle = styled.h1`
