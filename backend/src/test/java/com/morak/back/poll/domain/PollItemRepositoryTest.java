fbba7c2cbf2b1066799cc9c4efe1ebe373c82f49
cjlee38 <cjlee38@github.com>
2022-07-28 12:25:27 +0900

    refactor(poll): domain supplier 제거

--- backend/src/test/java/com/morak/back/poll/domain/PollItemRepositoryTest.java
+++ backend/src/test/java/com/morak/back/poll/domain/PollItemRepositoryTest.java
@@ -2,14 +2,9 @@ package com.morak.back.poll.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.morak.back.poll.support.DomainSupplier;
 import com.morak.back.support.RepositoryTest;
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -17,24 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 class PollItemRepositoryTest {
 
     @Autowired
-    private DomainSupplier supplier;
-
-    @Autowired
     private PollItemRepository pollItemRepository;
-
-    @Autowired
-    private EntityManagerFactory factory;
-    private EntityManager entityManager;
-
-    @BeforeEach
-    void setUp() {
-        entityManager = factory.createEntityManager();
-    }
 
     @Test
     void 투표_선택_항목을_저장한다() {
         // given
-        Poll poll = supplier.supplyPoll(1L);
+        Poll poll = Poll.builder()
+                .id(1L)
+                .build();
 
         List<PollItem> items = List.of(
                 PollItem.builder()
@@ -55,28 +40,6 @@ class PollItemRepositoryTest {
         // then
         assertThat(savedItems).allMatch(item -> item.getId() != null);
     }
-
-    @Test
-    void 투표_선택_항목의_투표_결과_리스트에서_투표_결과를_제거하면_DB에서_삭제된다() {
-        // given
-        EntityTransaction transaction = entityManager.getTransaction();
-        transaction.begin();
-
-        PollItem item = pollItemRepository.findById(2L).orElseThrow();
-
-        List<PollResult> pollResults = item.getPollResults();
-
-        pollResults.remove(0);
-
-        transaction.commit();
-
-        // when
-        PollItem findItem = pollItemRepository.findById(2L).orElseThrow();
-
-        // then
-        assertThat(findItem.getPollResults()).hasSize(1);
-    }
-
     @Test
     void 투표_id로_투표_선택_항목을_조회한다() {
         // given
--- backend/src/test/java/com/morak/back/poll/domain/PollRepositoryTest.java
+++ backend/src/test/java/com/morak/back/poll/domain/PollRepositoryTest.java
@@ -3,17 +3,12 @@ package com.morak.back.poll.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.morak.back.auth.domain.Member;
-import com.morak.back.poll.support.DomainSupplier;
 import com.morak.back.support.RepositoryTest;
 import com.morak.back.team.domain.Team;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,25 +16,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 class PollRepositoryTest {
 
     @Autowired
-    private DomainSupplier supplier;
-
-    @Autowired
     private PollRepository pollRepository;
-
-    @Autowired
-    private EntityManagerFactory factory;
-    private EntityManager entityManager;
-
-    @BeforeEach
-    void setUp() {
-        entityManager = factory.createEntityManager();
-    }
 
     @Test
     void 투표를_저장한다() {
         // given
-        Team team = supplier.supplyTeam(1L);
-        Member member = supplier.supplyMember(1L);
+        Team team = Team.builder()
+                .id(1L)
+                .build();
+        Member member = Member.builder()
+                .id(1L)
+                .build();
         Poll poll = Poll.builder()
                 .team(team)
                 .host(member)
@@ -58,7 +45,7 @@ class PollRepositoryTest {
         Assertions.assertAll(
                 () -> assertThat(savedPoll).isNotNull(),
                 () -> assertThat(savedPoll.getId()).isNotNull(),
-                () -> assertThat(savedPoll.getTitle()).isEqualTo("test-title"),
+                () -> assertThat(savedPoll.getTitle()).isEqualTo(savedPoll.getTitle()),
                 () -> assertThat(savedPoll.getTeam()).isNotNull(),
                 () -> assertThat(savedPoll.getHost()).isNotNull()
         );
@@ -76,34 +63,6 @@ class PollRepositoryTest {
         assertThat(polls).hasSize(1);
     }
 
-    @Test
-    void 투표를_저장할_때_선택항목도_저장한다() {
-        // given
-        EntityTransaction transaction = entityManager.getTransaction();
-        transaction.begin();
-
-        Poll poll = pollRepository.getById(1L);
-        poll.addItem(PollItem.builder()
-                .poll(poll)
-                .subject("test-subject-1")
-                .build());
-        poll.addItem(PollItem.builder()
-                .poll(poll)
-                .subject("test-subject-2")
-                .build());
-        // when
-        entityManager.flush();
-        entityManager.clear();
-
-        transaction.commit();
-
-        // then
-        Poll findPoll = pollRepository.getById(1L);
-        Assertions.assertAll(
-                () -> assertThat(findPoll.getPollItems()).hasSize(5),
-                () -> assertThat(findPoll.getPollItems().get(3).getSubject()).isEqualTo("test-subject-1")
-        );
-    }
 
     @Test
     void 투표_단건을_조회한다() {
@@ -120,7 +79,7 @@ class PollRepositoryTest {
     @Test
     void 잘못된_팀_id로_조회할_경우_null을_반환한다() {
         // given
-        Optional<Poll> poll = pollRepository.findByIdAndTeamId(1L, 2L);
+        Optional<Poll> poll = pollRepository.findByIdAndTeamId(999L, 999L);
 
         // when & then
         assertThat(poll).isEmpty();
