6de1c2e88fa914c3224938acf0272884ad2ae845
chalee <gptpem38@gmail.com>
2022-07-29 16:25:58 +0900

    refactor(poll): path에 투표 id 대신 code 사용 (#142)
    * refactor(poll): path에 투표 id 대신 code 사용
    
    * style: 주석처리한 코드 제거
    
    Co-authored-by: cjlee38 <cjlee38@github.com>

--- backend/src/test/java/com/morak/back/poll/domain/PollRepositoryTest.java
+++ backend/src/test/java/com/morak/back/poll/domain/PollRepositoryTest.java
@@ -35,7 +35,7 @@ class PollRepositoryTest {
                 .allowedPollCount(1)
                 .status(PollStatus.OPEN)
                 .closedAt(LocalDateTime.now().plusDays(1L))
-                .code("testcode")
+                .code("unique99")
                 .build();
 
         // when
@@ -67,7 +67,7 @@ class PollRepositoryTest {
     @Test
     void 투표_단건을_조회한다() {
         // given
-        Poll poll = pollRepository.findByIdAndTeamId(1L, 1L).orElseThrow();
+        Poll poll = pollRepository.findByCodeAndTeamId("testcode", 1L).orElseThrow();
 
         // when & then
         Assertions.assertAll(
@@ -77,9 +77,9 @@ class PollRepositoryTest {
     }
 
     @Test
-    void 잘못된_팀_id로_조회할_경우_null을_반환한다() {
+    void 잘못된_팀_code로_조회할_경우_null을_반환한다() {
         // given
-        Optional<Poll> poll = pollRepository.findByIdAndTeamId(999L, 999L);
+        Optional<Poll> poll = pollRepository.findByCodeAndTeamId("chaleeleeeee", 999L);
 
         // when & then
         assertThat(poll).isEmpty();
