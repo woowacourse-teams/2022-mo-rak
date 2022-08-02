3afc7b4eeb4d9e8f353eac34ca17a5b89134c9c5
cjlee38 <cjlee38@github.com>
2022-08-02 21:03:33 +0900

    feat(core): 예외처리 슬랙 알림 도메인 추가

--- backend/src/main/java/com/morak/back/MorakBackApplication.java
+++ backend/src/main/java/com/morak/back/MorakBackApplication.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class MorakBackApplication {
 
     public static void main(String[] args) {
