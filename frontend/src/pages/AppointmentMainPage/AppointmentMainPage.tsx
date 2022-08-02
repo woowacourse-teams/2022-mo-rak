ce093cb10d5aec4d5fbb73dd3d86b72d9ea496e7
sangminsong <ssmin940606@gmail.com>
2022-08-02 19:17:41 +0900

    feat(appointment): 약속 잡기 메인페이지 초기 UI 구현

--- frontend/src/pages/AppointmentMainPage/AppointmentMainPage.tsx
+++ frontend/src/pages/AppointmentMainPage/AppointmentMainPage.tsx
@@ -1,14 +1,6 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 
 function AppointmentMainPage() {
-  const navigate = useNavigate();
-
-  // 임시
-  useEffect(() => {
-    navigate('create');
-  }, []);
-
   return <div> 약속잡기 메인페이지입니다</div>;
 }
 
