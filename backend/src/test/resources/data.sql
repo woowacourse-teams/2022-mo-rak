4e19471f8f1db43d04f73b32c668e6876df018cf
leokim <kunga077@gmail.com>
2022-08-04 02:12:35 +0900

    chore(appointment): DB schema if exists 위치 변경

--- backend/src/test/resources/data.sql
+++ backend/src/test/resources/data.sql
@@ -1,15 +1,25 @@
-INSERT INTO member (oauth_id, name, profile_url, created_at, updated_at) values('12345678', 'eden', 'http://eden-profile.com', now(), now());
-INSERT INTO member (oauth_id, name, profile_url, created_at, updated_at) values('97533356', 'ellie', 'http://ellie-profile.com', now(), now());
-INSERT INTO member (oauth_id, name, profile_url, created_at, updated_at) values('12345679', 'bkr', 'http://bkr-profile.com', now(), now());
-INSERT INTO member (oauth_id, name, profile_url, created_at, updated_at) values('12345669', 'chalee', 'http://chalee-profile.com', now(), now());
-INSERT INTO member (oauth_id, name, profile_url, created_at, updated_at) values('12345668', 'albur', 'http://albur-profile.com', now(), now());
-INSERT INTO member (oauth_id, name, profile_url, created_at, updated_at) values('12345677', 'winnie', 'http://winnie-profile.com', now(), now());
+INSERT INTO member (oauth_id, name, profile_url, created_at, updated_at)
+values ('12345678', 'eden', 'http://eden-profile.com', now(), now());
+INSERT INTO member (oauth_id, name, profile_url, created_at, updated_at)
+values ('97533356', 'ellie', 'http://ellie-profile.com', now(), now());
+INSERT INTO member (oauth_id, name, profile_url, created_at, updated_at)
+values ('12345679', 'bkr', 'http://bkr-profile.com', now(), now());
+INSERT INTO member (oauth_id, name, profile_url, created_at, updated_at)
+values ('12345669', 'chalee', 'http://chalee-profile.com', now(), now());
+INSERT INTO member (oauth_id, name, profile_url, created_at, updated_at)
+values ('12345668', 'albur', 'http://albur-profile.com', now(), now());
+INSERT INTO member (oauth_id, name, profile_url, created_at, updated_at)
+values ('12345677', 'winnie', 'http://winnie-profile.com', now(), now());
 
-INSERT INTO team (name, code, created_at, updated_at) values('morak', 'MoraK123', now(), now());
+INSERT INTO team (name, code, created_at, updated_at)
+values ('morak', 'MoraK123', now(), now());
 
-INSERT INTO team_member (team_id, member_id, created_at, updated_at) values(1, 1, now(), now());
-INSERT INTO team_member (team_id, member_id, created_at, updated_at) values(1, 2, now(), now());
-INSERT INTO team_member (team_id, member_id, created_at, updated_at) values(1, 3, now(), now());
+INSERT INTO team_member (team_id, member_id, created_at, updated_at)
+values (1, 1, now(), now());
+INSERT INTO team_member (team_id, member_id, created_at, updated_at)
+values (1, 2, now(), now());
+INSERT INTO team_member (team_id, member_id, created_at, updated_at)
+values (1, 3, now(), now());
 
 INSERT INTO poll (team_id, host_id, title, allowed_poll_count, is_anonymous, status, created_at, updated_at, closed_at,
                   code)
@@ -36,14 +46,19 @@ values (2, 2, '', now(), now());
 INSERT INTO poll_result (poll_item_id, member_id, description, created_at, updated_at)
 values (3, 2, '에덴이 시켰어요', now(), now());
 
-INSERT INTO appointment (team_id, host_id, title, description, start_date, end_date, start_time, end_time, duration_minutes, status, code, closed_at, created_at, updated_at)
-VALUES (1, 2, '발표 준비 날짜 정하기', '데모 데이 발표 준비를 위한..', '2122-08-01', '2122-08-04', '13:00:00', '22:00:00', 60, 'OPEN', 'FEsd23C1', '2022-07-31T23:59:00', now(), now());
+INSERT INTO appointment (team_id, host_id, title, description, start_date, end_date, start_time, end_time,
+                         duration_minutes, status, code, closed_at, created_at, updated_at)
+VALUES (1, 2, '발표 준비 날짜 정하기', '데모 데이 발표 준비를 위한..', '2122-08-01', '2122-08-04', '13:00:00', '22:00:00', 60, 'OPEN',
+        'FEsd23C1', '2022-07-31T23:59:00', now(), now());
 
-INSERT INTO appointment_available_time (appointment_id, member_id, start_date_time, end_date_time, created_at, updated_at)
+INSERT INTO appointment_available_time (appointment_id, member_id, start_date_time, end_date_time, created_at,
+                                        updated_at)
 VALUES (1, 2, '2122-08-03T16:00:00', '2122-08-03T17:00:00', now(), now());
 
-INSERT INTO appointment_available_time (appointment_id, member_id, start_date_time, end_date_time, created_at, updated_at)
+INSERT INTO appointment_available_time (appointment_id, member_id, start_date_time, end_date_time, created_at,
+                                        updated_at)
 VALUES (1, 2, '2122-08-02T16:00:00', '2122-08-02T17:00:00', now(), now());
 
-INSERT INTO appointment_available_time (appointment_id, member_id, start_date_time, end_date_time, created_at, updated_at)
+INSERT INTO appointment_available_time (appointment_id, member_id, start_date_time, end_date_time, created_at,
+                                        updated_at)
 VALUES (1, 1, '2122-08-03T13:00:00', '2122-08-03T17:00:00', now(), now());
