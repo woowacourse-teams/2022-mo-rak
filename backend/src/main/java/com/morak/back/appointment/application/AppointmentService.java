c3e700c54c71f232b241619c97cf69a1d9958f11
leokim <kunga077@gmail.com>
2022-08-04 01:55:50 +0900

    feat(appointment): 약속 잡기 추천 서비스 로직 구현

--- backend/src/main/java/com/morak/back/appointment/application/AppointmentService.java
+++ backend/src/main/java/com/morak/back/appointment/application/AppointmentService.java
@@ -4,6 +4,8 @@ import com.morak.back.appointment.domain.Appointment;
 import com.morak.back.appointment.domain.AppointmentRepository;
 import com.morak.back.appointment.domain.AvailableTime;
 import com.morak.back.appointment.domain.AvailableTimeRepository;
+import com.morak.back.appointment.domain.RankRecommendation;
+import com.morak.back.appointment.domain.RecommendationCells;
 import com.morak.back.appointment.exception.AppointmentNotFoundException;
 import com.morak.back.appointment.ui.dto.AppointmentAllResponse;
 import com.morak.back.appointment.ui.dto.AppointmentCreateRequest;
@@ -19,13 +21,13 @@ import com.morak.back.core.domain.CodeGenerator;
 import com.morak.back.core.domain.RandomCodeGenerator;
 import com.morak.back.core.exception.InvalidRequestException;
 import com.morak.back.team.domain.Team;
+import com.morak.back.team.domain.TeamMember;
 import com.morak.back.team.domain.TeamMemberRepository;
 import com.morak.back.team.domain.TeamRepository;
 import com.morak.back.team.exception.MismatchedTeamException;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -86,12 +88,13 @@ public class AppointmentService {
         validateAppointmentStatus(appointment);
 
         availableTimeRepository.deleteAllByMemberIdAndAppointmentId(memberId, appointment.getId());
+        availableTimeRepository.flush();
 
         List<AvailableTime> availableTimes = requests.stream()
                 .map(request -> request.toAvailableTime(member, appointment))
                 .collect(Collectors.toList());
 
-        saveAllAvailableTimes(availableTimes);
+        availableTimeRepository.saveAll(availableTimes);
     }
 
     @Transactional(readOnly = true)
@@ -102,7 +105,19 @@ public class AppointmentService {
 
         Appointment appointment = appointmentRepository.findByCode(appointmentCode)
                 .orElseThrow(() -> new AppointmentNotFoundException(appointmentCode));
-        return null;
+        List<Member> members = teamMemberRepository.findAllByTeamId(teamId)
+                .stream()
+                .map(TeamMember::getMember)
+                .collect(Collectors.toList());
+
+        RecommendationCells recommender = RecommendationCells.of(appointment, members);
+
+        List<AvailableTime> availableTimes = availableTimeRepository.findAllByAppointmentId(appointment.getId());
+        List<RankRecommendation> rankRecommendations = recommender.recommend(availableTimes);
+
+        return rankRecommendations.stream()
+                .map(RecommendationResponse::from)
+                .collect(Collectors.toList());
     }
 
     public void closeAppointment(String teamCode, Long memberId, String appointmentCode) {
@@ -136,15 +151,6 @@ public class AppointmentService {
     private void validateAppointmentStatus(Appointment appointment) {
         if (appointment.isClosed()) {
             throw new InvalidRequestException(appointment.getId() + "번 약속잡기는 마감되었습니다.");
-        }
-    }
-
-    private void saveAllAvailableTimes(List<AvailableTime> availableTimes) {
-        // TODO: 2022/07/28 equals, hashcode를 오버라이드(아마 hashcode는 커스텀으로 해야할지도!) 하는 방법으로 변경해야함!!
-        try {
-            availableTimeRepository.saveAll(availableTimes);
-        } catch (DataIntegrityViolationException e) {
-            throw new InvalidRequestException("동일한 약속잡기 가능 시간을 선택할 수 없습니다.");
         }
     }
 }
--- backend/src/test/java/com/morak/back/appointment/application/AppointmentServiceTest.java
+++ backend/src/test/java/com/morak/back/appointment/application/AppointmentServiceTest.java
@@ -2,7 +2,9 @@ package com.morak.back.appointment.application;
 
 import static com.morak.back.appointment.domain.AppointmentStatus.CLOSED;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -11,17 +13,23 @@ import static org.mockito.Mockito.verify;
 import com.morak.back.appointment.FakeCodeGenerator;
 import com.morak.back.appointment.domain.Appointment;
 import com.morak.back.appointment.domain.AppointmentRepository;
+import com.morak.back.appointment.domain.AvailableTime;
 import com.morak.back.appointment.domain.AvailableTimeRepository;
 import com.morak.back.appointment.ui.dto.AppointmentAllResponse;
 import com.morak.back.appointment.ui.dto.AppointmentCreateRequest;
 import com.morak.back.appointment.ui.dto.AppointmentResponse;
+import com.morak.back.appointment.ui.dto.AvailableTimeRequest;
+import com.morak.back.appointment.ui.dto.RecommendationResponse;
 import com.morak.back.auth.domain.Member;
 import com.morak.back.auth.domain.MemberRepository;
 import com.morak.back.core.domain.Code;
+import com.morak.back.core.exception.InvalidRequestException;
 import com.morak.back.team.domain.Team;
+import com.morak.back.team.domain.TeamMember;
 import com.morak.back.team.domain.TeamMemberRepository;
 import com.morak.back.team.domain.TeamRepository;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
@@ -86,6 +94,27 @@ class AppointmentServiceTest {
             .code(Code.generate(new FakeCodeGenerator()))
             .build();
 
+    private final AvailableTime 회식_가능_시간_4시부터_4시반까지 = AvailableTime.builder()
+            .member(에덴)
+            .appointment(회식_날짜_약속잡기)
+            .startDateTime(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(16, 0)))
+            .endDateTime(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(16, 30)))
+            .build();
+
+    private final AvailableTime 회식_가능_시간_4시반부터_5시까지 = AvailableTime.builder()
+            .member(에덴)
+            .appointment(회식_날짜_약속잡기)
+            .startDateTime(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(16, 0)))
+            .endDateTime(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(16, 30)))
+            .build();
+
+    private final AvailableTime 회식_가능_시간_5시부터_5시반까지 = AvailableTime.builder()
+            .member(에덴)
+            .appointment(회식_날짜_약속잡기)
+            .startDateTime(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(16, 0)))
+            .endDateTime(LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(16, 30)))
+            .build();
+
     @Test
     void 약속잡기를_생성한다() {
         // given
@@ -139,6 +168,73 @@ class AppointmentServiceTest {
 
         // then
         assertThat(appointmentResponse.getTitle()).isEqualTo("회식 날짜");
+    }
+
+    @Test
+    void 약속잡기_가능시간을_선택한다() {
+        // given
+        given(memberRepository.findById(anyLong())).willReturn(Optional.of(에덴));
+        given(teamRepository.findIdByCode(anyString())).willReturn(Optional.of(모락.getId()));
+        given(teamMemberRepository.existsByTeamIdAndMemberId(anyLong(), anyLong())).willReturn(true);
+        given(appointmentRepository.findByCode(anyString())).willReturn(Optional.of(회식_날짜_약속잡기));
+
+        // when
+        String teamCode = "teamCode";
+        Long memberId = 에덴.getId();
+        String appointmentCode = "appointmentCode";
+        AvailableTimeRequest availableTimeRequest = new AvailableTimeRequest(
+                LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(16, 0)),
+                LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(16, 30))
+        );
+        List<AvailableTimeRequest> requests = List.of(availableTimeRequest);
+        appointmentService.selectAvailableTimes(teamCode, memberId, appointmentCode, requests);
+
+        // then
+        verify(availableTimeRepository).saveAll(anyList());
+    }
+
+    @Test
+    void 약속잡기_가능시간을_선택할_때_약속잡기가_이미_마감되었으면_예외를_던진다() {
+        // given
+        회식_날짜_약속잡기.close(에덴);
+        given(memberRepository.findById(anyLong())).willReturn(Optional.of(에덴));
+        given(teamRepository.findIdByCode(anyString())).willReturn(Optional.of(모락.getId()));
+        given(teamMemberRepository.existsByTeamIdAndMemberId(anyLong(), anyLong())).willReturn(true);
+        given(appointmentRepository.findByCode(anyString())).willReturn(Optional.of(회식_날짜_약속잡기));
+        // when
+        String teamCode = "teamCode";
+        Long memberId = 에덴.getId();
+        String appointmentCode = "appointmentCode";
+        AvailableTimeRequest availableTimeRequest = new AvailableTimeRequest(
+                LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(16, 0)),
+                LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(16, 30))
+        );
+        List<AvailableTimeRequest> requests = List.of(availableTimeRequest);
+
+        // then
+        assertThatThrownBy(() -> appointmentService.selectAvailableTimes(teamCode, memberId, appointmentCode, requests))
+                .isInstanceOf(InvalidRequestException.class)
+                .hasMessageContaining("번 약속잡기는 마감되었습니다.");
+    }
+
+    @Test
+    void 약속잡기_가능시간_추천_결과를_조회한다() {
+        // given
+        given(teamRepository.findIdByCode(anyString())).willReturn(Optional.of(모락.getId()));
+        given(teamMemberRepository.existsByTeamIdAndMemberId(anyLong(), anyLong())).willReturn(true);
+        given(appointmentRepository.findByCode(anyString())).willReturn(Optional.of(회식_날짜_약속잡기));
+        given(teamMemberRepository.findAllByTeamId(anyLong())).willReturn(List.of(new TeamMember(1L, 모락, 에덴)));
+
+        List<AvailableTime> availableTimes = List.of(회식_가능_시간_4시반부터_5시까지, 회식_가능_시간_4시반부터_5시까지,
+                회식_가능_시간_5시부터_5시반까지);
+        given(availableTimeRepository.findAllByAppointmentId(anyLong())).willReturn(availableTimes);
+
+        // when
+        List<RecommendationResponse> recommendationResponses = appointmentService.recommendAvailableTimes(모락.getCode(),
+                에덴.getId(), 회식_날짜_약속잡기.getCode());
+
+        // then
+        assertThat(recommendationResponses).hasSize(4);
     }
 
     @Test
