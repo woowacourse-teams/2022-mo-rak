4cb093e95ec742d5983daf314900c5447049d1f3
RIANAEH <ririhan217@gmail.com>
2022-08-03 15:21:11 +0900

    refactor(core): 리뷰 적극 반영

--- backend/src/main/java/com/morak/back/core/support/LogFormatter.java
+++ backend/src/main/java/com/morak/back/core/support/LogFormatter.java
@@ -3,6 +3,7 @@ package com.morak.back.core.support;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 
 public class LogFormatter {
@@ -10,29 +11,33 @@ public class LogFormatter {
     private static final String NEWLINE = System.getProperty("line.separator");
 
     public static String toPrettyRequestString(HttpServletRequest httpRequest) throws IOException {
-        StringBuilder stringBuilder = new StringBuilder(NEWLINE);
-        appendMethodAndURI(httpRequest, stringBuilder);
-        appendHeaders(httpRequest, stringBuilder);
-        appendBody(httpRequest, stringBuilder);
-        return stringBuilder.toString();
+        return NEWLINE + getMethodAndURI(httpRequest)
+                + getHeaders(httpRequest)
+                + getBody(httpRequest);
     }
 
-    private static void appendBody(HttpServletRequest httpRequest, StringBuilder stringBuilder) throws IOException {
-        BufferedReader reader = httpRequest.getReader();
-        stringBuilder.append("body: ").append(NEWLINE);
-        reader.lines().forEach(line -> stringBuilder.append(line).append(NEWLINE));
-    }
-
-    private static void appendHeaders(HttpServletRequest httpRequest, StringBuilder stringBuilder) {
+    private static StringBuilder getHeaders(HttpServletRequest httpRequest) {
+        StringBuilder stringBuilder = new StringBuilder();
         Enumeration<String> headerNames = httpRequest.getHeaderNames();
         while (headerNames.hasMoreElements()) {
             String headerName = headerNames.nextElement();
             stringBuilder.append(headerName).append(": ").append(httpRequest.getHeader(headerName)).append(NEWLINE);
         }
+        return stringBuilder;
     }
 
-    private static void appendMethodAndURI(HttpServletRequest httpRequest, StringBuilder stringBuilder) {
+    private static StringBuilder getMethodAndURI(HttpServletRequest httpRequest) {
+        StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("method: ").append(httpRequest.getMethod()).append(NEWLINE);
         stringBuilder.append("uri: ").append(httpRequest.getRequestURI()).append(NEWLINE);
+        return stringBuilder;
+    }
+
+    private static StringBuilder getBody(HttpServletRequest httpRequest) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader reader = httpRequest.getReader();
+        stringBuilder.append("body: ").append(NEWLINE);
+        stringBuilder.append(reader.lines().collect(Collectors.joining(NEWLINE)));
+        return stringBuilder;
     }
 }
