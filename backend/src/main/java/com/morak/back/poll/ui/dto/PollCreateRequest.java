8367dc2247e3f67a2393cf93e0d3af7ad3b817f2
cjlee38 <cjlee38@github.com>
2022-08-12 19:54:47 +0900

    refactor: JsonCreator 대신 No/AllArgsConstructor 사용

--- backend/src/main/java/com/morak/back/poll/ui/dto/PollCreateRequest.java
+++ backend/src/main/java/com/morak/back/poll/ui/dto/PollCreateRequest.java
@@ -1,6 +1,5 @@
 package com.morak.back.poll.ui.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.morak.back.auth.domain.Member;
 import com.morak.back.core.domain.Code;
 import com.morak.back.poll.domain.Poll;
@@ -11,38 +10,31 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.constraints.Future;
-import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PollCreateRequest {
 
     @NotBlank(message = "title은 blank일 수 없습니다.")
-    private final String title;
+    private String title;
 
     @NotNull(message = "allowedPollCount 는 null 일 수 없습니다.")
-    private final Integer allowedPollCount;
+    private Integer allowedPollCount;
 
     @NotNull(message = "isAnonymous 는 null 일 수 없습니다.")
-    private final Boolean isAnonymous;
+    private Boolean isAnonymous;
 
     @Future(message = "closedAt은 미래여야 합니다.")
-    private final LocalDateTime closedAt;
+    private LocalDateTime closedAt;
 
     @NotNull(message = "subjects 는 null 일 수 없습니다.")
-    private final List<String> subjects;
-
-    @JsonCreator
-    public PollCreateRequest(String title, Integer allowedPollCount, Boolean isAnonymous, LocalDateTime closedAt,
-                             List<String> subjects) {
-        this.title = title;
-        this.allowedPollCount = allowedPollCount;
-        this.isAnonymous = isAnonymous;
-        this.closedAt = closedAt;
-        this.subjects = subjects;
-    }
+    private List<String> subjects;
 
     public Poll toPoll(Member member, Team team, PollStatus status, Code code) {
         return Poll.builder()
