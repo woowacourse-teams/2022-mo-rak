f2aa2556f99620c23f6219200de253f9c794e95f
winnie_yeji <52344833+rladpwl0512@users.noreply.github.com>
2022-08-13 22:31:20 +0900

    feat: 약속잡기 메인페이지 구현  (#241)
    * feat(appointment): 약속잡기 메뉴를 클릭했을 때, 약속잡기 메인페이지로 이동
    
    * feat(appointment): 약속잡기 메인페이지 구현
    
    - 약속잡기 메인페이지 UI 구현
    - 약속잡기 메인페이지 도메인 구현
    
    * refactor(appointment): 필요없는 console 삭제
    
    * design(appointment): 약속 시간을 나타내는 태그 width 속성 변경
    
    * design(appointment): gap 속성 변경
    
    * design: 약속잡기 및 투표하기 메인페이지 header 스타일 통일
    
    - 투표하기 메인페이지에 title 추가, button 크기 변경, 구분선 추가
    
    * design: gap 속성 수정
    
    - 약속잡기 메인페이지와 같은 레이아웃으로 통일하기 위해, 투표하기 메인페이지의 gap도 같은 사이즈로 통일시켰습니다.
    
    * refactor(appointment): default 속성값 삭제
    
    * refactor(appointment): 마감 여부를 판단하는 로직 수정
    
    마감 여부를 판단하는 함수 삭제 및 return문 내부에서 마감 여부를 판단하도록 수정

---
+++ frontend/src/components/AppointmentMain/AppointmentMainDetail/AppointmentMainDetail.tsx
@@ -0,0 +1,44 @@
+import { useTheme } from '@emotion/react';
+import React from 'react';
+import styled from '@emotion/styled';
+
+import FlexContainer from '../../@common/FlexContainer/FlexContainer';
+import TextField from '../../@common/TextField/TextField';
+import { AppointmentInterface } from '../../../types/appointment';
+
+interface Props {
+  durationHours: AppointmentInterface['durationHours'];
+  durationMinutes: AppointmentInterface['durationMinutes'];
+}
+
+function AppointmentMainDetail({ durationHours, durationMinutes }: Props) {
+  const theme = useTheme();
+
+  return (
+    <FlexContainer gap="1.2rem">
+      <TextField
+        width="5.2rem"
+        borderRadius="20px"
+        variant="outlined"
+        padding="0.4rem 0"
+        colorScheme={theme.colors.PURPLE_100}
+      >
+        <StyledDetail>
+          {durationHours}
+          시간
+          {durationMinutes.toString().padStart(2, '0')}
+          분
+        </StyledDetail>
+      </TextField>
+    </FlexContainer>
+  );
+}
+
+const StyledDetail = styled.span(
+  ({ theme }) => `
+  color: ${theme.colors.PURPLE_100};
+  font-size: 0.8rem;
+`
+);
+
+export default AppointmentMainDetail;
