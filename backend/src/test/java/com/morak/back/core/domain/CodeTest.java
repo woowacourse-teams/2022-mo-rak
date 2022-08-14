0941372b6f29ae46eb909a0f8f7c0fb6c6fd905d
seong-wooo <seongwoo0513@uos.ac.kr>
2022-08-14 13:35:41 +0900

    refactor: 엘리의 세 번째 리뷰 반영

--- backend/src/test/java/com/morak/back/core/domain/CodeTest.java
+++ backend/src/test/java/com/morak/back/core/domain/CodeTest.java
@@ -2,9 +2,21 @@ package com.morak.back.core.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CodeTest {
+    @Test
+    void Code를_생성한다() {
+        // given
+        String randomCode = "하이하이하이하이";
+
+        //When
+        Code code = Code.generate((length) -> randomCode);
+
+        // then
+        assertThat(code.getCode()).isEqualTo(randomCode);
+    }
 
     @Test
     void 랜덤한_8자리의_Code를_생성한다() {
@@ -15,6 +27,6 @@ class CodeTest {
         Code code = Code.generate(randomCodeGenerator);
 
         // then
-        assertThat(code.getCode()).hasSize(8);
+        assertThat(code.getCode()).matches("[\\dA-z]{8}");
     }
 }
