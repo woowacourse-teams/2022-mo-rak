313a7119026bd31da2b8268cb7483fa09fe3203b
al-bur <64825713+al-bur@users.noreply.github.com>
2022-08-14 13:45:35 +0900

    feat(poll): 투표 마감시간을 설정할 때 당일 이전 날짜를 선택할 수 없도록 구현

--- frontend/src/components/PollCreate/PollCreateCloseTimeInput/PollCreateCloseTimeInput.tsx
+++ frontend/src/components/PollCreate/PollCreateCloseTimeInput/PollCreateCloseTimeInput.tsx
@@ -11,12 +11,21 @@ interface Props {
 }
 
 function PollCreateCloseTimeInput({ closingTime, closingDate, onChangeTime, onChangeDate }: Props) {
+  const [today] = new Date().toISOString().split('T');
+
   return (
     <FlexContainer justifyContent="end" alignItems="center">
       <FlexContainer flexDirection="column" alignItems="end" gap="0.4rem">
         {/* TODO: 하나의 label 여러개 input은 html에서 권장하지 않는 것이니 해결 필요 */}
         <StyledLabel>마감시간</StyledLabel>
-        <Input type="date" fontSize="1.6rem" value={closingDate} onChange={onChangeDate} required />
+        <Input
+          type="date"
+          fontSize="1.6rem"
+          value={closingDate}
+          min={today}
+          onChange={onChangeDate}
+          required
+        />
         <Input type="time" fontSize="1.6rem" value={closingTime} onChange={onChangeTime} required />
       </FlexContainer>
     </FlexContainer>
