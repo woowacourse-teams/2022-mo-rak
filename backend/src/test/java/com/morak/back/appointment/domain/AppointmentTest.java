4078bd14d3e1d49004e7576dbc74c42c0ffd9e96
leokim <kunga077@gmail.com>
2022-08-14 22:47:08 +0900

    chore: branch minimum 수정

--- backend/src/test/java/com/morak/back/appointment/domain/AppointmentTest.java
+++ backend/src/test/java/com/morak/back/appointment/domain/AppointmentTest.java
@@ -279,4 +279,16 @@ class AppointmentTest {
         // then
         assertThat(isAvailable).isFalse();
     }
+
+    @Test
+    void 자코코_테스트를_추가한다() {
+        // given
+        Appointment appointment = new Appointment();
+
+        // when
+
+
+        // then
+        assertThat(appointment.saySomething("HI")).isEqualTo("Hi guys!");
+    }
 }
