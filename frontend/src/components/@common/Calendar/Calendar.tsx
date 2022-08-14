7c9def3ff77378e28740fea89de452707580331a
al-bur <64825713+al-bur@users.noreply.github.com>
2022-08-14 13:28:01 +0900

    refactor: 캘린더 리팩토링 (#208)
    * feat: 이전 달, 다음 달 날짜를 클릭하면 해당 달로 이동할 수 있도록 구현
    
    - 단, 이전 달이 오늘(today)보다 이전이면 선택할 수 없도록 숨겨주었습니다.
    
    * refactor: 달력 컴포넌트 style 리팩토링
    
    * refactor: 달력 컴포넌트 props 타입 수정
    
    - Appointment type을 지우고 string으로 변경하면서, 강력하게 결합되 있는 것을 없애주었습니다.
    - Calendar 컴포넌트는 재사용 컴포넌트로 제작되어있기 때문에, 특정 컴포넌트와의 결합도는 제가해야 확장성을 높게 가져갈 수 있다고 생각했습니다.
    
    * refactor: 달력 날짜 범위 설정하는 로직 수정
    
    * refactor: 달력 version에 따라 렌더링 다르게 하는 로직 수정
    
    - 삼항 연산자 사용하여 가독성을 높이려 시도해보았다
    
    * refactor: semicolon 및 불필요한 개행 제거
    
    * refactor: 이번달을 나타내는 로직 함수로 추출

--- frontend/src/components/@common/Calendar/Calendar.tsx
+++ frontend/src/components/@common/Calendar/Calendar.tsx
@@ -1,17 +1,13 @@
 import React, { SetStateAction, useState, Dispatch } from 'react';
 import styled from '@emotion/styled';
-import { AppointmentInterface } from '../../../types/appointment';
 
 interface Props {
   version?: 'default' | 'select';
-  // NOTE: 이렇게 하면 Appointment에 의존하게 되어서 재사용이 불가능할듯?
-  startDate: AppointmentInterface['startDate'];
-  endDate: AppointmentInterface['endDate'];
-  // TODO: optional로 주는 게 맞을까?
-  setStartDate?: Dispatch<SetStateAction<AppointmentInterface['startDate']>>;
-  setEndDate?: Dispatch<SetStateAction<AppointmentInterface['endDate']>>;
-  // TODO: 임시 타이핑
+  startDate: string;
+  endDate: string;
   selectedDate?: string;
+  setStartDate?: Dispatch<SetStateAction<string>>;
+  setEndDate?: Dispatch<SetStateAction<string>>;
   setSelectedDate?: Dispatch<SetStateAction<string>>;
 }
 
@@ -20,11 +16,14 @@ const formatDate = (date: Date, day: number) =>
     .toString()
     .padStart(2, '0')}`;
 
+const weeks = ['일', '월', '화', '수', '목', '금', '토'];
+
 // version "default"는 약속 잡기 생성에서 사용된다. 기본 version이 default로 설정되어있기 때문에,
 // 별도로 props로 version을 넘겨주지 않아도 된다.
 // version "select"는 약속 잡기 선택하기에서 사용된다. 이때, props로 startDate, endDate가 넘어와야한다.
 // NOTE: version에 기반하여 컴포넌트가 둘로 나뉘어지기 때문에, 이는 두 가지 역할을 하나의 컴포넌트가 하는 게 아닐까 싶다.
 // 만약 version이 하나 더 생긴다면 로직이 복잡해질 것 같다. version을 없애는 게 첫 번째 리팩토링 포인트일듯
+// NOTE: 1차 리팩토링 끝
 // TODO: calendar 리팩토링
 function Calendar({
   version = 'default',
@@ -36,7 +35,6 @@ function Calendar({
   setSelectedDate
 }: Props) {
   const [date, setDate] = useState<Date>(new Date()); // 현재 날짜
-  const weeks = ['일', '월', '화', '수', '목', '금', '토'];
 
   // NOTE: UI - 여기 있어도 될듯, date에 의존적이다 - start
   const getPrevMonthDays = () => {
@@ -72,26 +70,20 @@ function Calendar({
   const handleStartOrEndDate = (day: number) => () => {
     // TODO: 약속잡기 진행페이지에서는 사용되지 않기 때문에, 분기처리를 해줌
     if (setStartDate && setEndDate) {
-      // 마지막 날이 선택 되어 있는 경우 -> 새로운 start 값을 설정해줘야함
-      if (endDate !== '') {
+      if (!startDate) {
+        setStartDate(formatDate(date, day));
+
+        return;
+      }
+
+      if (endDate || isBeforeStartDate(day)) {
         setEndDate('');
         setStartDate(formatDate(date, day));
-        return;
-      }
-      // 첫번째 날이 선택 되어 있는 경우 -> end값을 설정해줘야함
-      if (startDate !== '' && endDate === '') {
-        // end값을 설정해줘야하는데, start 날짜보다 앞일 때
-        if (isBeforeStartDate(day)) {
-          setEndDate(startDate);
-          setStartDate(formatDate(date, day));
 
-          return;
-        }
-        setEndDate(formatDate(date, day));
         return;
       }
-      // 가장 처음 값을 설정해줌 (위 두가지 조건을 충족하지 않은 경우)
-      setStartDate(formatDate(date, day));
+
+      setEndDate(formatDate(date, day));
     }
   };
 
@@ -130,13 +122,12 @@ function Calendar({
       .toString()
       .padStart(2, '0')}` === selectedDate;
 
+  const isThisMonth = () => new Date().getMonth() === date.getMonth();
+
   return (
     <StyledCalendar>
       <StyledMonth>
-        <StyledPrevButton
-          onClick={handleShowPrevMonth}
-          disabled={new Date().getMonth() === date.getMonth()}
-        >
+        <StyledPrevButton onClick={handleShowPrevMonth} disabled={isThisMonth()}>
           {/* TODO: 이런 방식이 괜찮은 방법인지 고민하기 */}
           &#8249;
         </StyledPrevButton>
@@ -151,58 +142,63 @@ function Calendar({
       <StyledDays>
         {/* 지난 달 일부 날짜 */}
         {getPrevMonthDays().map((day) => (
-          <StyledPrevMonthDays>{day}</StyledPrevMonthDays>
+          <StyledPrevMonthDay onClick={handleShowPrevMonth} isHidden={isThisMonth()}>
+            {day}
+          </StyledPrevMonthDay>
         ))}
 
         {/* 이번 달 날짜  */}
-        {getNowMonthDays().map((day) => {
-          if (version === 'select') {
-            if (isNotInStartAndEndDate(day)) {
-              return <StyledNowMonthDays type="notInStartAndEndDate">{day}</StyledNowMonthDays>;
-            }
+        {version === 'select'
+          ? getNowMonthDays().map((day) => {
+              if (isNotInStartAndEndDate(day)) {
+                return (
+                  <StyledNowMonthDayNotInStartAndEndDate>
+                    {day}
+                  </StyledNowMonthDayNotInStartAndEndDate>
+                );
+              }
 
-            return (
-              <StyledNowMonthDays
-                type="inStartAndEndDate"
-                onClick={handleSelectedDate(day)}
-                isSelectedDate={isSelectedDate(day)}
-              >
-                {day}
-              </StyledNowMonthDays>
-            );
-          }
+              return (
+                <StyledNowMonthDayInStartAndEndDate
+                  onClick={handleSelectedDate(day)}
+                  isSelectedDate={isSelectedDate(day)}
+                >
+                  {day}
+                </StyledNowMonthDayInStartAndEndDate>
+              );
+            })
+          : getNowMonthDays().map((day) => {
+              if (isPrevToday(day)) {
+                return <StyledNowMonthDayPrevToday>{day}</StyledNowMonthDayPrevToday>;
+              }
 
-          if (isPrevToday(day)) {
-            return <StyledNowMonthDays type="prevToday">{day}</StyledNowMonthDays>;
-          }
+              if (isToday(day)) {
+                return (
+                  <StyledNowMonthDayToday
+                    // TODO: 약속잡기 진행페이지에서는 사용되지 않기 때문에, 분기처리를 해줌
+                    onClick={setStartDate && setEndDate && handleStartOrEndDate(day)}
+                    isBetweenStartEndDate={isBetweenStartEndDate(day)}
+                    isStartOrEndDate={isStartOrEndDate(day)}
+                  >
+                    {day}
+                  </StyledNowMonthDayToday>
+                );
+              }
 
-          if (isToday(day)) {
-            return (
-              <StyledNowMonthDays
-                type="today"
-                // TODO: 약속잡기 진행페이지에서는 사용되지 않기 때문에, 분기처리를 해줌
-                onClick={setStartDate && setEndDate && handleStartOrEndDate(day)}
-                isBetweenStartEndDate={isBetweenStartEndDate(day)}
-                isStartOrEndDate={isStartOrEndDate(day)}
-              >
-                {day}
-              </StyledNowMonthDays>
-            );
-          }
-          return (
-            <StyledNowMonthDays
-              onClick={handleStartOrEndDate(day)}
-              isBetweenStartEndDate={isBetweenStartEndDate(day)}
-              isStartOrEndDate={isStartOrEndDate(day)}
-            >
-              {day}
-            </StyledNowMonthDays>
-          );
-        })}
+              return (
+                <StyledNowMonthDay
+                  onClick={handleStartOrEndDate(day)}
+                  isBetweenStartEndDate={isBetweenStartEndDate(day)}
+                  isStartOrEndDate={isStartOrEndDate(day)}
+                >
+                  {day}
+                </StyledNowMonthDay>
+              );
+            })}
 
         {/* 다음 달 일부 날짜  */}
         {getNextMonthDays().map((day) => (
-          <StyledNextMonthDays>{day}</StyledNextMonthDays>
+          <StyledNextMonthDay onClick={handleShowNextMonth}>{day}</StyledNextMonthDay>
         ))}
       </StyledDays>
     </StyledCalendar>
@@ -219,7 +215,6 @@ const StyledCalendar = styled.div(
 );
 
 const StyledMonth = styled.div`
-  width: 100%;
   height: 12rem;
   display: flex;
   justify-content: space-between;
@@ -229,7 +224,6 @@ const StyledMonth = styled.div`
 `;
 
 const StyledWeekends = styled.div`
-  width: 100%;
   height: 5.2rem;
   padding: 0 0.4rem;
   display: flex;
@@ -258,76 +252,65 @@ const StyledMonthTitle = styled.h1`
 `;
 
 const StyledDays = styled.div`
-  width: 100%;
   display: flex;
   flex-wrap: wrap;
   padding: 0.4rem;
   cursor: pointer;
 `;
 
-// TODO: StyledPrevMonthDays, StyledNextMonthDays, StyledNowMonthDays에 공통적인 스타일이 들어감
+const StyledMonthDay = styled.div`
+  font-size: 1.6rem;
+  margin: 0.3rem;
+  width: calc(40.2rem / 7);
+  height: calc(40.2rem / 7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.2s;
+`;
+
 // (4단위로 맞지 않는 부분이 있는데, 4단위로 맞추면 깨지는 이슈...)
-const StyledPrevMonthDays = styled.div(
-  ({ theme }) => `
-  color: ${theme.colors.WHITE_100}; // TODO: 현재 달력에서는 이전달, 다음달 날짜 일부를 보여줄 필요가 없어서 화면에서 보이지 않도록 처리. (이후 필요할 시, props로 option을 받아서 보이도록 해줄 수 있음)
-  font-size: 1.6rem;
-  margin: 0.3rem; 
-  width: calc(40.2rem / 7);
-  height: calc(40.2rem / 7);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: background-color 0.2s;
+// TODO: 현재 달력에서는 이전달, 다음달 날짜 일부를 보여줄 필요가 없어서 화면에서 보이지 않도록 처리.
+// (이후 필요할 시, props로 option을 받아서 보이도록 해줄 수 있음)
+const StyledPrevMonthDay = styled(StyledMonthDay)<{ isHidden: boolean }>(
+  ({ theme, isHidden }) => `
+  color: ${theme.colors.GRAY_300};
+  ${isHidden && 'visibility: hidden'};
 `
 );
 
-const StyledNextMonthDays = styled.div(
+const StyledNextMonthDay = styled(StyledMonthDay)(
   ({ theme }) => `
-  color: ${theme.colors.WHITE_100};
-  font-size: 1.6rem;
-  margin: 0.3rem; 
-  width: calc(40.2rem / 7);
-  height: calc(40.2rem / 7);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: background-color 0.2s;
+  color: ${theme.colors.GRAY_300};
 `
 );
 
-const StyledNowMonthDays = styled.div<{
+const StyledNowMonthDay = styled(StyledMonthDay)<{
   isBetweenStartEndDate?: boolean;
   isStartOrEndDate?: boolean;
   isSelectedDate?: boolean;
-  type?: string;
 }>(
-  ({ theme, isBetweenStartEndDate, isStartOrEndDate, isSelectedDate, type }) => `
+  ({ theme, isBetweenStartEndDate, isStartOrEndDate, isSelectedDate }) => `
   color: ${theme.colors.BLACK_100};
-  font-size: 1.6rem;
-  margin: 0.3rem; 
-  width: calc(40.2rem / 7);
-  height: calc(40.2rem / 7);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: background-color 0.3s;
 
   &:hover {
     border: 2px solid ${theme.colors.PURPLE_100};
     border-radius: 100%;
   }
-
-  ${type === 'prevToday' ? `color: ${theme.colors.GRAY_200};` : ''}
-
+  
   ${
-    type === 'today' ? `color: ${theme.colors.BLACK_100};` : '' // 현재는 일반 날짜와 다른 것이 없지만, 이후 today에 대한 스타일이 필요하면 속성 추가 가능
+    isSelectedDate
+      ? `background-color: ${theme.colors.PURPLE_100};
+    color: ${theme.colors.WHITE_100};
+    border-radius: 100%;`
+      : ''
   }
 
   ${
     isStartOrEndDate
       ? `background-color: ${theme.colors.PURPLE_100};
-    color: ${theme.colors.WHITE_100};
-    border-radius: 100%;`
+  color: ${theme.colors.WHITE_100};
+  border-radius: 100%;`
       : ''
   }
 
@@ -338,28 +321,29 @@ const StyledNowMonthDays = styled.div<{
     `
       : ''
   }
+`
+);
 
-  // version이 select일 때
+const StyledNowMonthDayPrevToday = styled(StyledNowMonthDay)(
+  ({ theme }) => `
+  color: ${theme.colors.GRAY_200};
+`
+);
 
-  ${
-    type === 'notInStartAndEndDate'
-      ? `color: ${theme.colors.GRAY_200};
+const StyledNowMonthDayToday = styled(StyledNowMonthDay)``;
+
+const StyledNowMonthDayNotInStartAndEndDate = styled(StyledNowMonthDay)(
+  ({ theme }) => `
+  color: ${theme.colors.GRAY_200};
   &:hover {
     border: none;
   }
-  `
-      : ''
-  }
+`
+);
 
-  ${type === 'inStartAndEndDate' ? `color: ${theme.colors.BLACK_100};` : ''}
-
-  ${
-    isSelectedDate
-      ? `background-color: ${theme.colors.PURPLE_100};
-    color: ${theme.colors.WHITE_100};
-    border-radius: 100%;`
-      : ''
-  }
+const StyledNowMonthDayInStartAndEndDate = styled(StyledNowMonthDay)(
+  ({ theme }) => `
+  color: ${theme.colors.BLACK_100};
 `
 );
 
