8d88e3e4dd39713978888f99ad2fe3f2d49bce46
KIM YEJI <rladpwl0512@gmail.com>
2022-08-16 03:06:47 +0900

    feat(modal): 모달 재사용 컴포넌트 구현

---
+++ frontend/src/components/@common/Modal/Modal.tsx
@@ -0,0 +1,43 @@
+import React, { MouseEvent, PropsWithChildren, useRef } from 'react';
+import styled from '@emotion/styled';
+
+interface Props extends PropsWithChildren {
+  visible: boolean;
+  onClose: () => void;
+}
+
+function Modal({ children, visible, onClose }: Props) {
+  const outside = useRef<HTMLDivElement>(null);
+
+  const handleModalClose = (e: MouseEvent<HTMLDivElement>) => {
+    if (outside.current === e.target) {
+      onClose();
+    }
+  };
+
+  return (
+    <StyledModalContainer
+      ref={outside}
+      visible={visible}
+      tabIndex={-1}
+      onClick={handleModalClose}
+    >
+      {children}
+    </StyledModalContainer>
+  );
+}
+
+const StyledModalContainer = styled.div<{visible: boolean}>(({ theme, visible }) => `
+  z-index: 10;
+  display: ${visible ? 'flex' : 'none'};
+  background-color: ${theme.colors.TRANSPARENT_BLACK_100_25};
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+`);
+
+export default Modal;
