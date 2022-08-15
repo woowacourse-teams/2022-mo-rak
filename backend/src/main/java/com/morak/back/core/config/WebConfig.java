d416529b849e59ba1674144aad3edf55f30ea21c
SWpark <79205414+seong-wooo@users.noreply.github.com>
2022-08-15 15:24:02 +0900

    feat: 존재하지 않는 API 요청 시 커스텀 예외 응답 (#247)
    * feat(core): 존재하지 않는 API 요청 시 커스텀 예외 응답
    
    * refactor (core): 에덴의 리뷰 반영
    
    * refactor(core): 엘리의 리뷰 반영

--- backend/src/main/java/com/morak/back/core/config/WebConfig.java
+++ backend/src/main/java/com/morak/back/core/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.morak.back.core.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .exposedHeaders(HttpHeaders.LOCATION);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/docs/**")
+                .addResourceLocations("classpath:/static/docs/");
     }
 }
