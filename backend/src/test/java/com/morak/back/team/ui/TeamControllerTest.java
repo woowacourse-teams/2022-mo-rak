40e983b4b32dbcaf35d828bde3c04bc651129244
cjlee38 <cjlee38@github.com>
2022-08-16 14:02:03 +0900

    test(team): TeamControllerTest가 ControllerTest를 상속하도록 변경

--- backend/src/test/java/com/morak/back/team/ui/TeamControllerTest.java
+++ backend/src/test/java/com/morak/back/team/ui/TeamControllerTest.java
@@ -16,9 +16,8 @@ import static org.springframework.restdocs.request.RequestDocumentation.pathPara
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.morak.back.auth.application.TokenProvider;
 import com.morak.back.auth.ui.dto.MemberResponse;
+import com.morak.back.poll.ui.ControllerTest;
 import com.morak.back.team.application.TeamService;
 import com.morak.back.team.ui.dto.InvitationJoinedResponse;
 import com.morak.back.team.ui.dto.TeamCreateRequest;
@@ -26,30 +25,15 @@ import com.morak.back.team.ui.dto.TeamResponse;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(TeamController.class)
-@AutoConfigureRestDocs
-@AutoConfigureDataJpa
-class TeamControllerTest {
+class TeamControllerTest extends ControllerTest {
 
     @MockBean
     private TeamService teamService;
-
-    @MockBean
-    private TokenProvider tokenProvider;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
