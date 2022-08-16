64962985df896a614960d78177c9ff70c9e93fda
cjlee38 <cjlee38@github.com>
2022-08-16 12:25:23 +0900

    refactor(core): 사용하지 않는 예외 제거 및 테스트 수정

--- backend/src/test/java/com/morak/external/RestSlackClientTest.java
+++ backend/src/test/java/com/morak/external/RestSlackClientTest.java
@@ -1,14 +1,11 @@
 package com.morak.external;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
-import com.morak.back.core.domain.slack.FakeApiReceiver;
-import com.morak.back.core.domain.slack.FakeSlackClient;
+import com.morak.back.core.domain.slack.RestSlackClient;
 import com.morak.back.core.domain.slack.SlackClient;
 import com.morak.back.core.domain.slack.SlackWebhook;
 import com.morak.back.team.domain.Team;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -19,14 +16,11 @@ public class RestSlackClientTest {
     void 실제_모락_슬랙에_마감_알림을_보낸다() {
         // given
         String url = System.getenv("SLACK_ERROR_URL");
-        FakeApiReceiver receiver = new FakeApiReceiver();
-        SlackClient client = new FakeSlackClient(receiver);
+        SlackClient client = new RestSlackClient();
+
         // when
         SlackWebhook webhook = new SlackWebhook(1L, new Team(), url);
         // then
-        Assertions.assertAll(
-                () -> assertThatNoException().isThrownBy(() -> client.notifyClosed(webhook, "hi")),
-                () -> assertThat(receiver.getMessage()).contains("슬랙 API를 요청합니다")
-        );
+        assertThatNoException().isThrownBy(() -> client.notifyClosed(webhook, "test-in-backend-test-code"));
     }
 }
