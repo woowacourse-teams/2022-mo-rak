6cef38cedcf9db3b67a06d08d34f99ee83703180
winnie_yeji <52344833+rladpwl0512@users.noreply.github.com>
2022-08-17 17:39:15 +0900

    refactor: 페이지 내부 멤버 프로필 조회를 삭제 (#291)

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,20 +1,14 @@
 import styled from '@emotion/styled';
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import MembersProfile from '../../components/MembersProfile/MembersProfile';
 import MainFeatureMenuContainer from '../../components/Main/MainFeatureMenuContainer/MainFeatureMenuContainer';
-import { GroupInterface } from '../../types/group';
 import { useMenuDispatch } from '../../context/MenuProvider';
 
 function MainPage() {
   const dispatch = useMenuDispatch();
   dispatch({ type: 'SET_CLICKED_MENU', menu: 'main' });
 
-  const { groupCode } = useParams() as { groupCode: GroupInterface['code'] };
-
   return (
     <StyledContainer>
-      <MembersProfile groupCode={groupCode} />
       <MainFeatureMenuContainer />
     </StyledContainer>
   );
