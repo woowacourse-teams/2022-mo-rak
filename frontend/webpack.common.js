bd27b6c6441782f5d456918a0259565adbdf71c1
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-02 14:48:39 +0900

    chore: 불필요한 babel-loader preset 제거
    Co-authored-by: winnie_yeji <rladpwl0512@users.noreply.github.com>

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -38,7 +38,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
