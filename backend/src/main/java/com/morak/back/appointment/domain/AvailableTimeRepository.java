1f9801c7c663c1874971c7790ecf8220db086b8c
cjlee38 <cjlee38@github.com>
2022-09-15 11:47:16 +0900

    refactor: repository 쿼리 개선

--- backend/src/main/java/com/morak/back/appointment/domain/AvailableTimeRepository.java
+++ backend/src/main/java/com/morak/back/appointment/domain/AvailableTimeRepository.java
@@ -2,7 +2,10 @@ package com.morak.back.appointment.domain;
 
 import com.morak.back.auth.domain.Member;
 import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 public interface AvailableTimeRepository extends Repository<AvailableTime, Long> {
 
@@ -12,11 +15,15 @@ public interface AvailableTimeRepository extends Repository<AvailableTime, Long>
 
     List<AvailableTime> findAllByMemberAndAppointment(Member member, Appointment appointment); // 사용하지 않음
 
-    void deleteAllByMemberAndAppointment(Member member, Appointment appointment);
+    @Modifying
+    @Query("DELETE FROM AvailableTime at WHERE at.member = :member and at.appointment = :appointment")
+    void deleteAllByMemberAndAppointment(@Param("member") Member member, @Param("appointment") Appointment appointment);
 
     List<AvailableTime> findAllByAppointment(Appointment appointment);
 
     void flush();
 
-    void deleteAllByAppointment(Appointment appointment);
+    @Modifying
+    @Query("DELETE FROM AvailableTime at WHERE at.appointment = :appointment")
+    void deleteAllByAppointment(@Param("appointment") Appointment appointment);
 }
