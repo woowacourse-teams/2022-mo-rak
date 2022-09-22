626bba4e9baefdf6c838f6a1d490541c1e012557
al-bur <64825713+al-bur@users.noreply.github.com>
2022-09-22 22:34:16 +0900

    refactor(appointment): 불필요한 fragment 제거

--- frontend/src/components/AppointmentCreate/AppointmentCreateHeader/AppointmentCreateHeader.tsx
+++ frontend/src/components/AppointmentCreate/AppointmentCreateHeader/AppointmentCreateHeader.tsx
@@ -1,11 +1,7 @@
 import styled from '@emotion/styled';
 
 function AppointmentCreateHeader() {
-  return (
-    <>
-      <StyledHeader>약속을 생성해주세요 🥳</StyledHeader>
-    </>
-  );
+  return <StyledHeader>약속을 생성해주세요 🥳</StyledHeader>;
 }
 
 const StyledHeader = styled.header`
