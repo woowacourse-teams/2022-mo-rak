78604c5b41d7b129f186b4a16028676a6d08d2ff
al-bur <64825713+al-bur@users.noreply.github.com>
2022-10-01 12:33:33 +0900

    refactor(poll): 재투표 및 재투표 결과 확인에 대한 테스트 리팩토링

--- frontend/cypress/e2e/poll.cy.ts
+++ frontend/cypress/e2e/poll.cy.ts
@@ -11,7 +11,6 @@ describe('투표 기능에 대한 e2e 테스트', () => {
 
   it('메인페이지에 접속할 수 있다.', () => {
     cy.visit('http://localhost:3000/');
-    cy.url().should('contain', '/groups/');
     cy.wait('@getGroupMembers');
     cy.wait('@getGroups');
   });
@@ -23,11 +22,9 @@ describe('투표 기능에 대한 e2e 테스트', () => {
     const currentHourMinute = currentTime.slice(0, 5);
 
     cy.findByRole('img', { name: 'poll-menu' }).click();
-    cy.url().should('contain', '/poll');
     cy.wait('@getPolls');
 
     cy.findByRole('button', { name: '투표 생성하기' }).click();
-    cy.url().should('contain', '/poll/create');
     cy.findByRole('textbox', { name: 'poll-title' }).type('점심 뭐먹지?');
     cy.findByRole('textbox', { name: 'poll-closingDate' }).type(dateAfter7days);
     cy.findByRole('textbox', { name: 'poll-closingTime' }).type(currentHourMinute);
@@ -36,11 +33,10 @@ describe('투표 기능에 대한 e2e 테스트', () => {
 
     cy.findByRole('button', { name: '투표 만들기' }).click();
     cy.wait('@createPoll');
-    cy.wait('@getPollItems');
   });
 
   it('투표를 진행할 수 있다.', () => {
-    cy.url().should('contain', '/progress');
+    cy.wait('@getPollItems');
     cy.findByText('서브웨이').click();
     cy.findByRole('textbox', { name: '서브웨이-description' }).type(
       '서브웨이가 세상에서 제일 좋아요!'
@@ -49,24 +45,34 @@ describe('투표 기능에 대한 e2e 테스트', () => {
   });
 
   it('투표 결과를 확인할 수 있다.', () => {
-    cy.url().should('contain', '/result');
     cy.wait('@getPollResult');
     cy.findByRole('generic', { name: 'poll-result-서브웨이' });
     cy.findByRole('generic', { name: '서브웨이' });
     cy.findByRole('generic', { name: '서브웨이-count' }).should('have.text', 1);
   });
 
-  // it('재투표를 할 수 있다.', () => {
-  //   cy.findByText('재투표하기').click();
-  //   cy.findByText('공단').click();
-  //   cy.findByRole('textbox', { name: '공단' }).click().type('사실 공단이 세상에서 제일 좋아요!');
-  //   cy.findByText('투표하기').click();
-  //   cy.wait('@getPollResult');
-  // });
+  it('재투표를 할 수 있다.', () => {
+    cy.findByRole('button', { name: '재투표하기' }).click();
+    cy.wait('@getPollItems');
 
-  // it('재투표 결과를 확인할 수 있다.', () => {
-  //   cy.get('[aria-label="공단-result"]').findByText('1').should('exist'); // TODO: 다른 방식으로 get해오는 방식
-  // });
+    cy.findByText('공단').click();
+    cy.findByRole('textbox', { name: '공단-description' }).type(
+      '사실 공단이 세상에서 제일 좋아요!'
+    );
+    cy.findByRole('button', { name: '투표하기' }).click();
+  });
+
+  it('바뀐 투표 결과를 확인할 수 있다.', () => {
+    cy.wait('@getPollResult');
+
+    cy.findByRole('generic', { name: 'poll-result-서브웨이' });
+    cy.findByRole('generic', { name: '서브웨이' });
+    cy.findByRole('generic', { name: '서브웨이-count' }).should('have.text', 0);
+
+    cy.findByRole('generic', { name: 'poll-result-공단' });
+    cy.findByRole('generic', { name: '공단' });
+    cy.findByRole('generic', { name: '공단-count' }).should('have.text', 1);
+  });
 
   // it('투표를 마감할 수 있다.', () => {
   //   cy.findByText('투표 마감하기').click();
