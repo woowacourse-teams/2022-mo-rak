2491c57ada6950164f5f292509b2aa90e0be96b8
al-bur <64825713+al-bur@users.noreply.github.com>
2022-10-03 09:16:33 +0900

    chore: cypress 기본 viewport 설정

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   projectId: 'vs3n5w',
   e2e: {},
-
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   env: {
     'cypress-react-selector': {
       root: '#root'
