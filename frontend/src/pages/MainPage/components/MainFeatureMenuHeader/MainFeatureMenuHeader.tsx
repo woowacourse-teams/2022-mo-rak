49f084a32055acb14f98bffb9966ef533849f582
KIM YEJI <rladpwl0512@gmail.com>
2022-10-10 18:28:59 +0900

    style: 필요없는 주석 삭제

--- frontend/src/pages/MainPage/components/MainFeatureMenuHeader/MainFeatureMenuHeader.tsx
+++ frontend/src/pages/MainPage/components/MainFeatureMenuHeader/MainFeatureMenuHeader.tsx
@@ -4,7 +4,6 @@ import { StyledTitle } from './MainFeatureMenuHeader.styles';
 function MainFeatureMenuHeader() {
   return (
     <>
-      {/* TODO: header 컴포넌트로 따로 분리해야할까? */}
       <StyledTitle>사용할 기능을 선택하세요!</StyledTitle>
       <Divider />
     </>
