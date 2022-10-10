967c22f4f89b57621b9c9a0e0bd8de2867c55a9d
KIM YEJI <rladpwl0512@gmail.com>
2022-10-10 18:41:00 +0900

    feat: 랜딩페이지의 서비스 소개 섹션을 모바일 화면에 대응

--- frontend/src/pages/LandingPage/components/LandingServiceIntroductionSection/LandingServiceIntroductionSection.tsx
+++ frontend/src/pages/LandingPage/components/LandingServiceIntroductionSection/LandingServiceIntroductionSection.tsx
@@ -1,10 +1,11 @@
 import { HTMLAttributes } from 'react';
 import {
   StyledHighlightImage,
-  StyledServiceIntroductionSection,
+  StyledContainer,
   StyledServicesImage,
   StyledSubTitle,
-  StyledTitle
+  StyledTitle,
+  StyledTitleContainer
 } from './LandingServiceIntroductionSection.styles';
 
 import Services from '../../../../assets/services.svg';
@@ -15,9 +16,9 @@ interface Props extends HTMLAttributes<HTMLElement> {}
 
 function LandingServiceIntroductionSection({ id }: Props) {
   return (
-    <StyledServiceIntroductionSection id={id}>
+    <StyledContainer id={id}>
       <LandingNavbar />
-      <div>
+      <StyledTitleContainer>
         <StyledTitle>
           분산된 서비스...
           <br />
@@ -25,13 +26,15 @@ function LandingServiceIntroductionSection({ id }: Props) {
           <StyledHighlightImage src={CircleHighlight} alt="highlight" />
         </StyledTitle>
         <StyledSubTitle>
-          투표 하기, 일정 정하기... 분산 되어있는 서비스에 불편함을 느끼지 않으셨나요?
+          투표 하기, 일정 정하기...
+          <br />
+          분산 되어있는 서비스에 불편함을 느끼지 않으셨나요?
           <br />
           모락에 모두 모여있어요!
         </StyledSubTitle>
-      </div>
+      </StyledTitleContainer>
       <StyledServicesImage src={Services} alt="services" />
-    </StyledServiceIntroductionSection>
+    </StyledContainer>
   );
 }
 
