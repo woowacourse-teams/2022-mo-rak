315557bb932c1a8b4be9aa7a3ef812a7623a4ae2
al-bur <64825713+al-bur@users.noreply.github.com>
2022-10-10 21:06:08 +0900

    feat: 그룹 생성 및 참가 에러 핸들링 추가 구현

--- frontend/src/pages/GroupInitPage/components/GroupParticipateForm/GroupParticipateForm.tsx
+++ frontend/src/pages/GroupInitPage/components/GroupParticipateForm/GroupParticipateForm.tsx
@@ -8,6 +8,7 @@ import GroupParticipateInvitationCodeInput from '../GroupParticipateInvitationCo
 import GroupParticipateFormSubmitButton from '../GroupPariticipateFormSubmitButton/GroupParticipateFormSubmitButton';
 import FlexContainer from '../../../../components/FlexContainer/FlexContainer';
 import { StyledForm } from './GroupParticipateForm.style';
+import { AxiosError } from 'axios';
 
 function GroupParticipateForm() {
   const [invitationCode, handleInvitationCode] = useInput('');
@@ -17,12 +18,24 @@ function GroupParticipateForm() {
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    if (!invitationCode) {
+      alert('그룹 코드를 입력해주세요!');
+
+      return;
+    }
+
     try {
       const res = await participateGroup(invitationCode);
       const groupCode = res.headers.location.split('/groups/')[1];
       navigate(`/groups/${groupCode}`);
     } catch (err) {
-      console.log(err);
+      if (err instanceof AxiosError) {
+        const errCode = err.response?.data.codeNumber;
+
+        if (errCode === '1301') {
+          alert('그룹 코드를 다시 한 번 확인해주세요!');
+        }
+      }
     }
   };
 
