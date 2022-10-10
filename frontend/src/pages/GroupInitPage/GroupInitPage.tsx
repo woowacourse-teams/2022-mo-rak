2247b9804ff6b2b65cc78c15ba58187724255543
al-bur <64825713+al-bur@users.noreply.github.com>
2022-10-10 20:56:35 +0900

    refactor: 그룹 생성/참가 페이지 api 요청 리팩토링
    - 함수를 따로 정의해주는 것보다, 즉시 실행 함수로 바꾸어주었습니다. 사용되지 않는 함수이기 때문!

--- frontend/src/pages/GroupInitPage/GroupInitPage.tsx
+++ frontend/src/pages/GroupInitPage/GroupInitPage.tsx
@@ -3,34 +3,34 @@ import { useNavigate } from 'react-router-dom';
 
 import GroupInitContainer from './components/GroupInitContainer/GroupInitContainer';
 import { getDefaultGroup } from '../../api/group';
-import { getLocalStorageItem, removeLocalStorageItem } from '../../utils/storage';
+import { getLocalStorageItem } from '../../utils/storage';
 import { StyledContainer } from './GroupInitPage.style';
+import { AxiosError } from 'axios';
 
 function GroupInitPage() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchGetDefaultGroup = async () => {
-      try {
-        const res = await getDefaultGroup();
-        const { code: groupCode } = res.data;
-
-        navigate(`/groups/${groupCode}`);
-      } catch (err) {
-        if (err instanceof Error) {
-        }
-        console.log(
-          '그룹 생성 및 참가 페이지에서 로그인을 했지만, 속해있는 그룹이 없는 것을 확인했습니다.'
-        );
-      }
-    };
-
-    // TODO: 중복로직 해결 필요
-    // TODO: 다시 한 번 token을 가져오는 로직 개선필요
-    const token = getLocalStorageItem('token');
-
+    // TODO: 중복 로직해결
+    // TODO: 다시 한 번 token을 가져오는 중복 로직해결
+    const token = getLocalStorageItem<string>('token');
     if (token) {
-      fetchGetDefaultGroup();
+      (async () => {
+        try {
+          const res = await getDefaultGroup();
+          const { code: groupCode } = res.data;
+
+          navigate(`/groups/${groupCode}`);
+        } catch (err) {
+          if (err instanceof AxiosError) {
+            const statusCode = err.response?.status;
+
+            if (statusCode === 404) {
+              navigate('/init');
+            }
+          }
+        }
+      })();
     }
   }, []);
 
