007851e39af97165bf4a784848c4f278a26b7871
al-bur <64825713+al-bur@users.noreply.github.com>
2022-10-11 23:40:58 +0900

    refactor: 사이드바 불필요한 코드 제거

--- frontend/src/layouts/SidebarLayout/components/Sidebar/Sidebar.tsx
+++ frontend/src/layouts/SidebarLayout/components/Sidebar/Sidebar.tsx
@@ -17,13 +17,11 @@ import SidebarSlackMenu from '../SidebarSlackMenu/SidebarSlackMenu';
 import SidebarLogoutMenu from '../SidebarLogoutMenu/SidebarLogoutMenu';
 
 function Sidebar() {
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
   const [groups, setGroups] = useState<Array<GroupInterface>>([]);
   const [activeModalMenu, setActiveModalMenu] = useState<null | string>(null);
-
   const { groupCode } = useParams() as { groupCode: GroupInterface['code'] };
-
-  const navigate = useNavigate();
 
   const handleNavigate = (location: string) => () => {
     navigate(location);
@@ -34,50 +32,46 @@ function Sidebar() {
   };
 
   useEffect(() => {
-    const fetchGroups = async () => {
+    (async () => {
       try {
         const res = await getGroups();
         setGroups(res.data);
         setIsLoading(false);
       } catch (err) {
-        alert(err);
         setIsLoading(true);
       }
-    };
-
-    fetchGroups();
+    })();
   }, [groupCode]);
+
+  if (isLoading) return <div>로딩중</div>;
 
   return (
     <>
       <StyledContainer>
-        {isLoading ? (
-          <div>로딩중</div>
-        ) : (
-          <>
-            <StyledLogo src={Logo} alt={Logo} onClick={handleNavigate(`/groups/${groupCode}`)} />
-            <SidebarGroupsMenu
-              onClickMenu={handleActiveModalMenu}
-              groupCode={groupCode}
-              groups={groups}
-            />
+        <>
+          <StyledLogo src={Logo} alt={Logo} onClick={handleNavigate(`/groups/${groupCode}`)} />
+          <SidebarGroupsMenu
+            onClickMenu={handleActiveModalMenu}
+            groupCode={groupCode}
+            groups={groups}
+          />
 
-            <Divider />
-            <SidebarFeatureMenu groupCode={groupCode} />
+          <Divider />
+          <SidebarFeatureMenu groupCode={groupCode} />
 
-            <Divider />
-            <SidebarMembersProfileMenu groupCode={groupCode} />
+          <Divider />
+          <SidebarMembersProfileMenu groupCode={groupCode} />
 
-            <StyledBottomMenu>
-              <SidebarSlackMenu onClick={handleActiveModalMenu('slack')} />
-              <SidebarInvitationMenu groupCode={groupCode} />
-              <SidebarLogoutMenu />
-            </StyledBottomMenu>
-          </>
-        )}
+          <StyledBottomMenu>
+            <SidebarSlackMenu onClick={handleActiveModalMenu('slack')} />
+            <SidebarInvitationMenu groupCode={groupCode} />
+            <SidebarLogoutMenu />
+          </StyledBottomMenu>
+        </>
       </StyledContainer>
 
       {/* TODO: 모달이 모여있음  */}
+      {/* TODO: portal 사용 */}
       <SidebarMenuModals
         activeModalMenu={activeModalMenu}
         closeModal={handleActiveModalMenu(null)}
--- frontend/src/layouts/SidebarLayout/components/SidebarMembersProfileMenu/SidebarMembersProfileMenu.tsx
+++ frontend/src/layouts/SidebarLayout/components/SidebarMembersProfileMenu/SidebarMembersProfileMenu.tsx
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled';
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { getGroupMembers } from '../../../../api/group';
 import { GroupInterface, MemberInterface } from '../../../../types/group';
 import Avatar from '../../../../components/Avatar/Avatar';
@@ -11,30 +10,14 @@ interface Props {
 }
 
 function SidebarMembersProfileMenu({ groupCode }: Props) {
-  const navigate = useNavigate();
   const [groupMembers, setGroupMembers] = useState<Array<MemberInterface>>([]);
   const groupMembersCount = groupMembers.length;
 
   useEffect(() => {
-    const fetchGroupMembers = async () => {
-      try {
-        if (groupCode) {
-          const res = await getGroupMembers(groupCode);
-
-          setGroupMembers(res.data);
-        }
-      } catch (err) {
-        if (err instanceof Error) {
-          const statusCode = err.message;
-          if (statusCode === ('404' || '400')) {
-            alert('그룹이 없어요~');
-            navigate('/');
-          }
-        }
-      }
-    };
-
-    fetchGroupMembers();
+    (async () => {
+      const res = await getGroupMembers(groupCode);
+      setGroupMembers(res.data);
+    })();
   }, [groupCode]);
 
   return (
