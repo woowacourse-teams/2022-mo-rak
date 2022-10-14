a7015ab60bd659593c2022f7cb21c812f092e6ef
cjlee38 <cjlee38@github.com>
2022-10-15 01:36:36 +0900

    refactor: 스켈레톤 코드에 팀멤버 레포지토리 추가

--- backend/src/main/java/com/morak/back/role/application/RoleService.java
+++ backend/src/main/java/com/morak/back/role/application/RoleService.java
@@ -1,6 +1,17 @@
 package com.morak.back.role.application;
 
+import com.morak.back.auth.domain.Member;
+import com.morak.back.auth.domain.MemberRepository;
+import com.morak.back.auth.exception.MemberNotFoundException;
+import com.morak.back.core.exception.CustomErrorCode;
+import com.morak.back.role.domain.Role;
 import com.morak.back.role.domain.RoleRepository;
+import com.morak.back.team.domain.Team;
+import com.morak.back.team.domain.TeamMemberRepository;
+import com.morak.back.team.domain.TeamRepository;
+import com.morak.back.team.exception.TeamAuthorizationException;
+import com.morak.back.team.exception.TeamNotFoundException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +22,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class RoleService {
 
     private final RoleRepository roleRepository;
+    private final MemberRepository memberRepository;
+    private final TeamRepository teamRepository;
+    private final TeamMemberRepository teamMemberRepository;
 
     // -- A
 
@@ -18,7 +32,23 @@ public class RoleService {
 
     // -- B
 
+    public void editRoleNames(String teamCode, Long memberId, List<String> names) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> MemberNotFoundException.of(CustomErrorCode.MEMBER_NOT_FOUND_ERROR, memberId));
+        Team team = teamRepository.findByCode(teamCode)
+                .orElseThrow(() -> TeamNotFoundException.ofTeam(CustomErrorCode.TEAM_NOT_FOUND_ERROR, teamCode));
+        validateMemberInTeam(team, member);
 
+        Role role = roleRepository.findByTeamCode(teamCode).orElseThrow();
+        role.updateNames(names);
+    }
+
+    private void validateMemberInTeam(Team team, Member member) {
+        if (!teamMemberRepository.existsByTeamAndMember(team, member)) {
+            throw TeamAuthorizationException.of(CustomErrorCode.TEAM_MEMBER_MISMATCHED_ERROR, team.getId(),
+                    member.getId());
+        }
+    }
 
     // -- C
 
--- backend/src/test/java/com/morak/back/role/application/RoleServiceTest.java
+++ backend/src/test/java/com/morak/back/role/application/RoleServiceTest.java
@@ -2,8 +2,11 @@ package com.morak.back.role.application;
 
 import static org.assertj.core.api.Assertions.*;
 
+import com.morak.back.auth.domain.MemberRepository;
 import com.morak.back.role.domain.RoleRepository;
 import com.morak.back.support.ServiceTest;
+import com.morak.back.team.domain.TeamMemberRepository;
+import com.morak.back.team.domain.TeamRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,12 +14,23 @@ import org.junit.jupiter.api.Test;
 class RoleServiceTest {
 
     private final RoleRepository roleRepository;
+    private final MemberRepository memberRepository;
+    private final TeamRepository teamRepository;
+    private final TeamMemberRepository teamMemberRepository;
 
-    public RoleServiceTest(RoleRepository roleRepository) {
+    private final RoleService roleService;
+
+    public RoleServiceTest(RoleRepository roleRepository,
+                           MemberRepository memberRepository,
+                           TeamRepository teamRepository,
+                           TeamMemberRepository teamMemberRepository) {
         this.roleRepository = roleRepository;
+        this.memberRepository = memberRepository;
+        this.teamRepository = teamRepository;
+        this.teamMemberRepository = teamMemberRepository;
+
+        this.roleService = new RoleService(roleRepository, memberRepository, teamRepository, teamMemberRepository);
     }
-
-
 
     // -- A
 
@@ -24,6 +38,14 @@ class RoleServiceTest {
 
     // -- B
 
+    @Test
+    void 역할의_이름_목록을_수정한다() {
+        // given
+
+        // when
+
+        // then
+    }
 
 
     // -- C
