61354c2f6a8d0d2cdd8095adb2c310981520559e
KIM YEJI <rladpwl0512@gmail.com>
2022-10-15 04:25:53 +0900

    refactor: 컴포넌트 속성 추가
    - flex-wrap 추가
    
    Co-authored-by: al-bur <al-bur@users.noreply.github.com>

--- frontend/src/components/FlexContainer/FlexContainer.styles.tsx
+++ frontend/src/components/FlexContainer/FlexContainer.styles.tsx
@@ -2,12 +2,13 @@ import { CSSProperties } from 'react';
 import styled from '@emotion/styled';
 
 const StyledContainer = styled.div<CSSProperties>(
-  ({ flexDirection, alignItems, justifyContent, gap }) => `
+  ({ flexDirection, alignItems, justifyContent, gap, flexWrap }) => `
   display: flex;
   flex-direction: ${flexDirection || 'row'};
   ${alignItems && `align-items: ${alignItems}`};
   ${justifyContent && `justify-content: ${justifyContent}`};
   ${gap && `gap: ${gap}`};
+  ${flexWrap && `flex-wrap: ${flexWrap}`};
 `
 );
 
