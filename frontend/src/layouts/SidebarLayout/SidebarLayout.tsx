bb0d1d21517ff1fe3a3753e1865b2a92c3cbe2c7
al-bur <64825713+al-bur@users.noreply.github.com>
2022-10-16 21:25:21 +0900

    refactor: 그룹 멤버에 대한 데이터를 전역에서 사용할 수 있도록 구현

--- frontend/src/layouts/SidebarLayout/SidebarLayout.tsx
+++ frontend/src/layouts/SidebarLayout/SidebarLayout.tsx
@@ -2,13 +2,16 @@ import { Outlet } from 'react-router-dom';
 import FlexContainer from '../../components/FlexContainer/FlexContainer';
 import Sidebar from './components/Sidebar/Sidebar';
 import { MenuProvider } from '../../context/MenuProvider';
+import { GroupMembersProvider } from '../../context/GroupMembersProvider';
 
 function SidebarLayout() {
   return (
     <FlexContainer>
       <MenuProvider>
-        <Sidebar />
-        <Outlet />
+        <GroupMembersProvider>
+          <Sidebar />
+          <Outlet />
+        </GroupMembersProvider>
       </MenuProvider>
     </FlexContainer>
   );
