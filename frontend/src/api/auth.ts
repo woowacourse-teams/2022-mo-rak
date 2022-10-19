2e46b2129e48d8eec5d17ee421f5f9e4a455487a
al-bur <64825713+al-bur@users.noreply.github.com>
2022-10-19 21:24:04 +0900

    refactor: 타입 네이밍 변경

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,9 +1,9 @@
-import { EditUserNameRequest } from '../types/auth';
+import { EditUsernameRequest } from '../types/auth';
 import { publicAuthAxiosInstance, privateAuthAxiosInstance } from './axios';
 
 const signin = (code: string) => publicAuthAxiosInstance.post('/signin', code);
 const getUser = () => privateAuthAxiosInstance.get('/me');
-const editUserName = (userName: EditUserNameRequest) =>
+const editUsername = (userName: EditUsernameRequest) =>
   privateAuthAxiosInstance.patch('/me/name', userName);
 
-export { signin, getUser, editUserName };
+export { signin, getUser, editUsername };
