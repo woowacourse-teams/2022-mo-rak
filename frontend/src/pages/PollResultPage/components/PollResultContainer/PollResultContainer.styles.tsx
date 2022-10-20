e7e2bffa741e0ab56db125a4807289970ae56eaa
KIM YEJI <rladpwl0512@gmail.com>
2022-10-21 06:34:26 +0900

    feat: 투표 결과 페이지 tooltip 추가

--- frontend/src/pages/PollResultPage/components/PollResultContainer/PollResultContainer.styles.tsx
+++ frontend/src/pages/PollResultPage/components/PollResultContainer/PollResultContainer.styles.tsx
@@ -11,4 +11,32 @@ const StyledLoadingContainer = styled.div`
   height: 60rem;
 `;
 
-export { StyledTitle, StyledLoadingContainer };
+const StyledHelpIconContainer = styled.div(
+  ({ theme }) => `
+  display: flex;
+  justify-content: center;
+  width: 2rem;
+  height: 2rem;
+  background: ${theme.colors.GRAY_300};
+  border-radius: 100%;
+`
+);
+
+const StyledHelpIcon = styled.img`
+  width: 1.6rem;
+`;
+
+const StyledDescription = styled.div(
+  ({ theme }) => `
+  color: ${theme.colors.GRAY_400};
+  font-size: 1.6rem;
+`
+);
+
+export {
+  StyledTitle,
+  StyledLoadingContainer,
+  StyledHelpIconContainer,
+  StyledHelpIcon,
+  StyledDescription
+};
