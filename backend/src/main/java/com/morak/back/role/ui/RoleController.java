866f3ac5e29b93e449be57d4cf6cc81b72f7df7a
RIANAEH <ririhan217@gmail.com>
2022-10-20 20:36:44 +0900

    refactor(role): 불필요한 validated 어노테이션 제거

--- backend/src/main/java/com/morak/back/role/ui/RoleController.java
+++ backend/src/main/java/com/morak/back/role/ui/RoleController.java
@@ -9,7 +9,6 @@ import java.net.URI;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Validated
 @RequestMapping("/api/groups/{groupCode}/roles")
 public class RoleController {
 
