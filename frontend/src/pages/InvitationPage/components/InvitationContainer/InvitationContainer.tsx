bb404572c3cc18dda7d60ab5b0e81f5bd78fe055
al-bur <64825713+al-bur@users.noreply.github.com>
2022-10-21 02:24:04 +0900

    fix: 잘못된 초대 링크로 접속시 무한 로딩 문제 해결

--- frontend/src/pages/InvitationPage/components/InvitationContainer/InvitationContainer.tsx
+++ frontend/src/pages/InvitationPage/components/InvitationContainer/InvitationContainer.tsx
@@ -17,7 +17,7 @@ function InvitationContainer() {
   const { invitationCode } = useParams() as { invitationCode: string };
 
   useEffect(() => {
-    const fetchGetIsJoinedGroup = async () => {
+    (async () => {
       try {
         const res = await getIsJoinedGroup(invitationCode);
         const { groupCode, name, isJoined } = res.data;
@@ -32,16 +32,18 @@ function InvitationContainer() {
         setIsLoading(false);
       } catch (err) {
         if (err instanceof AxiosError) {
+          const errCode = err.response?.data.codeNumber;
+
+          if (errCode === '1301') {
+            alert('유효하지 않은 초대장입니다');
+            navigate('/');
+          }
         }
 
         setIsLoading(true);
       }
-    };
-
-    fetchGetIsJoinedGroup();
+    })();
   }, []);
-
-  if (isLoading) return <div>로딩중</div>;
 
   return (
     <Box width="60rem" padding="9.2rem 0">
@@ -52,17 +54,23 @@ function InvitationContainer() {
         gap="6.6rem"
       >
         <StyledLogo src={Logo} alt="logo" />
-        <StyledTitle>
-          {name}
-          그룹으로 초대합니다
-        </StyledTitle>
-        <InvitationButtonGroup
-          // NOTE: useParams으로 code들을 다 내려주고 있는데, navigate만 내려주지 않고 자식 컴포넌트에서
-          // 다시 호출해서 navigate를 만드는 게 맞을까?
-          navigate={navigate}
-          invitationCode={invitationCode}
-          groupCode={groupCode}
-        />
+        {!isLoading ? (
+          <>
+            <StyledTitle>
+              {name}
+              그룹으로 초대합니다
+            </StyledTitle>
+            <InvitationButtonGroup
+              // NOTE: useParams으로 code들을 다 내려주고 있는데, navigate만 내려주지 않고 자식 컴포넌트에서
+              // 다시 호출해서 navigate를 만드는 게 맞을까?
+              navigate={navigate}
+              invitationCode={invitationCode}
+              groupCode={groupCode}
+            />
+          </>
+        ) : (
+          <div>로딩중</div>
+        )}
       </FlexContainer>
     </Box>
   );
