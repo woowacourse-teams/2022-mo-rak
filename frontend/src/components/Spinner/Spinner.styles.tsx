22c5c68ee8d2a62010e407c5bdbdefc13d8d73ef
al-bur <64825713+al-bur@users.noreply.github.com>
2022-10-20 13:25:07 +0900

    feat: spinner 컴포넌트 구현

---
+++ frontend/src/components/Spinner/Spinner.styles.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export { StyledContainer };
---
+++ frontend/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,17 @@
+import { useLottie } from 'lottie-react';
+import { CSSProperties } from 'react';
+import spinnerAnimation from '../../assets/spinner.json';
+import { StyledContainer } from './Spinner.styles';
+
+type Props = {
+  width: CSSProperties['width'];
+};
+
+function Spinner({ width }: Props) {
+  // TODO: lottie 네이밍 통일
+  const spinnerLottie = useLottie({ animationData: spinnerAnimation }, { width });
+
+  return <StyledContainer>{spinnerLottie.View}</StyledContainer>;
+}
+
+export default Spinner;
