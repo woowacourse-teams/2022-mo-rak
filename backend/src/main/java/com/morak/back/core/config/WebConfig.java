38d50bb064879aa1af675ed1f30f627e38fd2a13
leokim <kunga077@gmail.com>
2022-10-20 18:56:58 +0900

    chore: prometheus 설정

--- backend/src/main/java/com/morak/back/core/config/WebConfig.java
+++ backend/src/main/java/com/morak/back/core/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.morak.back.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,11 +12,25 @@ public class WebConfig implements WebMvcConfigurer {
 
     public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
+    private final String allowedOrigin;
+    private final String prometheusOrigin;
+
+    public WebConfig(@Value("${cors.allowed-origin}") String allowedOrigin,
+                     @Value("${monitoring.prometheus.origin}") String prometheusOrigin) {
+        this.allowedOrigin = allowedOrigin;
+        this.prometheusOrigin = prometheusOrigin;
+    }
+
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .exposedHeaders(HttpHeaders.LOCATION);
+
+        registry.addMapping("/actuator/**")
+                .allowedOrigins(prometheusOrigin)
+                .allowedMethods("GET", "HEAD", "OPTIONS");
     }
 
     @Override
