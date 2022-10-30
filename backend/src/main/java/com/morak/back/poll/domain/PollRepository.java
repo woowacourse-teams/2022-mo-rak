97d67d694c95bbb591c453ce1c5515e7ed2bb6bb
RIANAEH <ririhan217@gmail.com>
2022-10-31 04:51:00 +0900

    refactor(poll): JpaRepository -> Repository

--- backend/src/main/java/com/morak/back/poll/domain/PollRepository.java
+++ backend/src/main/java/com/morak/back/poll/domain/PollRepository.java
@@ -3,18 +3,22 @@ package com.morak.back.poll.domain;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 
-public interface PollRepository extends JpaRepository<Poll, Long> {
+public interface PollRepository extends Repository<Poll, Long> {
 
-    @Query("select p from Poll p where p.menu.code.code = :code")
+    Poll save(Poll poll);
+
+    @Query("SELECT p FROM Poll p WHERE p.menu.code.code = :code")
     Optional<Poll> findByCode(@Param("code") String code);
 
-    @Query("select p from Poll p inner join Team t on t.code = p.menu.teamCode where p.menu.status = 'OPEN' and p.menu.closedAt.closedAt <= :thresholdDateTime")
+    @Query("SELECT p FROM Poll p INNER JOIN Team t ON t.code = p.menu.teamCode WHERE p.menu.status = 'OPEN' AND p.menu.closedAt.closedAt <= :thresholdDateTime")
     List<Poll> findAllToBeClosed(@Param("thresholdDateTime") LocalDateTime thresholdDateTime);
 
-    @Query("select p from Poll p where p.menu.teamCode.code = :teamCode")
+    @Query("SELECT p FROM Poll p WHERE p.menu.teamCode.code = :teamCode")
     List<Poll> findAllByTeamCode(@Param("teamCode") String teamCode);
+
+    void delete(Poll poll);
 }
