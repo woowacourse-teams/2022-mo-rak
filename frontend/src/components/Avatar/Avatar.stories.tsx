d72577cd90202ea56ff537681c6d18ec76b8c1dd
al-bur <64825713+al-bur@users.noreply.github.com>
2022-11-01 22:30:23 +0900

    feat: Avatar 컴포넌트 스토리 추가
    - 이름 없는 아바타 컴포넌트

--- frontend/src/components/Avatar/Avatar.stories.tsx
+++ frontend/src/components/Avatar/Avatar.stories.tsx
@@ -8,18 +8,22 @@ export default {
 } as Meta;
 
 const DefaultTemplate: Story = (args) => (
+  <Avatar profileUrl="https://avatars.githubusercontent.com/u/64825713?v=4" {...args} />
+);
+const Default = DefaultTemplate.bind({});
+
+const NamedTemplate: Story = (args) => (
   <Avatar
     profileUrl="https://avatars.githubusercontent.com/u/64825713?v=4"
     name="al-bur"
     {...args}
   />
 );
-const Default = DefaultTemplate.bind({});
+const Named = NamedTemplate.bind({});
 
 const NoImageFoundTemplate: Story = (args) => (
   <Avatar profileUrl="not-found" name="al-bur" {...args} />
 );
-
 const NoImageFound = NoImageFoundTemplate.bind({});
 
-export { Default, NoImageFound };
+export { Default, Named, NoImageFound };
