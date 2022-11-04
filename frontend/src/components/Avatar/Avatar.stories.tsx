74c6a5a3334fa95564c59fda735c8fe7e8c84332
al-bur <64825713+al-bur@users.noreply.github.com>
2022-11-04 15:25:42 +0900

    feat: 재사용 컴포넌트에 대한 storybook 추가 생성 (#556)
    * feat: Avatar 컴포넌트 스토리 작성
    
    * refactor: 전반적인 스토리 리팩토링
    
    - 네이밍 통일
    - 부족한 default props 제공
    
    * refactor: 전체 스토리 export 포맷 변경
    
    - 맨 밑에서 한 꺼번에 모아서 export하는 방법으로 변경 => 해당 스토리 파일에서 export 하는 것을 한 번에 모아서 보여주는 것이 더 가독성 있다고 생각했습니다!
    
    * feat: Avatar 컴포넌트 스토리 추가
    
    - 이름 없는 아바타 컴포넌트
    
    * feat: FlexContainer 컴포넌트 스토리 생성
    
    * feat: MarginContainer 컴포넌트 스토리 생성
    
    * feat: Modal 컴포넌트 스토리 생성
    
    * feat: Spinner 컴포넌트 스토리 생성
    
    * feat: Tooltip 컴포넌트 스토리 생성
    
    * chore: package.json 수정
    
    * refactor: 컴포넌트명 수정
    
    * refactor: 불필요한 코드 삭제
    
    * refactor: 불필요한 코드 삭제

---
+++ frontend/src/components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, Story } from '@storybook/react';
+
+import Avatar from './Avatar';
+
+export default {
+  title: 'Reusable Components/Avatar',
+  component: Avatar
+} as Meta;
+
+const DefaultTemplate: Story = (args) => (
+  <Avatar profileUrl="https://avatars.githubusercontent.com/u/64825713?v=4" {...args} />
+);
+const Default = DefaultTemplate.bind({});
+
+const NamedTemplate: Story = (args) => (
+  <Avatar
+    profileUrl="https://avatars.githubusercontent.com/u/64825713?v=4"
+    name="al-bur"
+    {...args}
+  />
+);
+const Named = NamedTemplate.bind({});
+
+const NoImageFoundTemplate: Story = (args) => (
+  <Avatar profileUrl="not-found" name="al-bur" {...args} />
+);
+const NoImageFound = NoImageFoundTemplate.bind({});
+
+export { Default, Named, NoImageFound };
