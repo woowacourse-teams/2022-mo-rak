013cf01c8230077a15f6616d895f11a7a3bd9ed7
winnie_yeji <52344833+rladpwl0512@users.noreply.github.com>
2022-11-06 19:56:36 +0900

    feat: 그룹 생성 및 참가 페이지의 접근성 개선  (#571)
    * feat: 모락 로고에 대한 접근성 개선
    
    * refactor: 접근성 개선을 위한 시맨틱 태그 사용
    
    * refactor: 접근성 개선을 위한 줄바꿈 태그 삭제
    
    * refactor: 컴포넌트명 변경
    
    * feat: 장식 아이콘에 대해 스크린리더기가 무시하도록 속성 추가

--- frontend/src/pages/GroupInitPage/components/GroupInitContainer/GroupInitContainer.styles.tsx
+++ frontend/src/pages/GroupInitPage/components/GroupInitContainer/GroupInitContainer.styles.tsx
@@ -31,12 +31,12 @@ const StyledLogo = styled.img`
   margin-bottom: 4rem;
 `;
 
-const StyledBigText = styled.div`
+const StyledTitle = styled.h1`
   font-size: 4.4rem;
   margin-bottom: 2rem;
 `;
 
-const StyledSmallText = styled.div`
+const StyledDescription = styled.p`
   font-size: 2.4rem;
   padding: 0 4rem;
   text-align: center;
@@ -48,6 +48,6 @@ export {
   StyledTopContainer,
   StyledBottomContainer,
   StyledLogo,
-  StyledBigText,
-  StyledSmallText
+  StyledTitle,
+  StyledDescription
 };
