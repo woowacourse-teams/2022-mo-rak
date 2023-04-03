07192a19d3f23bc0bec7f6cdce1bf025e86ab8e6
leokim <kunga077@gmail.com>
2023-04-04 00:50:09 +0900

    refactor: 쿼리 개수 하나로 줄이는 로직으로 변경

--- backend/src/main/java/com/morak/back/role/domain/RoleEntityRepositoryImpl.java
+++ backend/src/main/java/com/morak/back/role/domain/RoleEntityRepositoryImpl.java
@@ -1,88 +1,56 @@
 package com.morak.back.role.domain;
 
-import com.morak.back.core.domain.Code;
-import java.util.ArrayList;
+import static com.morak.back.role.domain.QRole.role;
+import static com.morak.back.role.domain.QRoleHistory.roleHistory;
+
+import com.querydsl.core.types.dsl.DateTemplate;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import javax.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class RoleEntityRepositoryImpl implements RoleEntityRepository {
 
-    private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final JPAQueryFactory jpaQueryFactory;
 
-    public RoleEntityRepositoryImpl(NamedParameterJdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public RoleEntityRepositoryImpl(EntityManager entityManager) {
+        this.jpaQueryFactory = new JPAQueryFactory(entityManager);
     }
 
     @Override
     public Optional<Role> findWithOnlyHistoriesPerDate(String teamCode) {
-        Role role = queryRole(teamCode);
-        if (role == null) {
-            return Optional.empty();
-        }
-        RoleHistories roleHistories = findRoleHistories(role.getId());
+        RoleHistories roleHistories = findRoleHistories(teamCode);
         Role roleWithHistories = new Role(teamCode, null, roleHistories);
         return Optional.of(roleWithHistories);
     }
 
-    private Role queryRole(String teamCode) {
-        try {
-            return jdbcTemplate.queryForObject(
-                    "SELECT * FROM role r WHERE r.team_code = :teamCode",
-                    new MapSqlParameterSource("teamCode", teamCode),
-                    (rs, rowNum) -> new Role(rs.getLong("id"), Code.generate(ignored -> teamCode), null, null)
-            );
-        } catch (DataAccessException e) {
-            return null;
-        }
-    }
-
-    private RoleHistories findRoleHistories(Long roleId) {
-        List<RoleHistory> roleHistories = queryRoleHistories(roleId);
-        if (roleHistories.isEmpty()) {
-            return new RoleHistories(roleHistories);
-        }
-        Map<Long, RoleHistory> idsByHistory = roleHistories.stream()
-                .collect(Collectors.toMap(RoleHistory::getId, Function.identity()));
-        addMatchResults(idsByHistory);
+    private RoleHistories findRoleHistories(String teamCode) {
+        List<RoleHistory> roleHistories = jpaQueryFactory.selectFrom(roleHistory)
+                .join(roleHistory.matchResults).fetchJoin()
+                .where(roleHistory.id.in(
+                        JPAExpressions.select(roleHistory.id.max())
+                                .from(roleHistory)
+                                .groupBy(toLocalDate())
+                                .orderBy(toLocalDate().desc())
+                                .where(roleHistory.roleId.eq(
+                                        JPAExpressions.select(role.id)
+                                                .from(role)
+                                                .where(role.teamCode.code.eq(teamCode))
+                                ))
+                ))
+                .fetch();
         return new RoleHistories(roleHistories);
     }
 
-    private List<RoleHistory> queryRoleHistories(Long roleId) {
-        return jdbcTemplate.query(
-                "SELECT MAX(ID) id, MAX(rh.date_time) date_time "
-                        + "FROM role_history rh "
-                        + "WHERE rh.role_id = :roleId "
-                        + "GROUP BY cast(rh.date_time as DATE) "
-                        + "ORDER BY date_time desc",
-                new MapSqlParameterSource("roleId", roleId),
-                (rs, rowNum) -> new RoleHistory(
-                        rs.getLong("id"),
-                        rs.getTimestamp("date_time").toLocalDateTime(),
-                        new ArrayList<>()
-                )
-        );
-    }
-
-    private void addMatchResults(Map<Long, RoleHistory> idsByHistory) {
-        jdbcTemplate.query(
-                "SELECT * FROM role_match_result rms WHERE rms.role_history_id in (:ids)",
-                new MapSqlParameterSource("ids", idsByHistory.keySet()),
-                (rs, rowNum) -> {
-                    RoleMatchResult matchResult = new RoleMatchResult(
-                            new RoleName(rs.getString("role_name")),
-                            rs.getLong("member_id")
-                    );
-                    RoleHistory history = idsByHistory.get(rs.getLong("role_history_id"));
-                    history.getMatchResults().add(matchResult);
-                    return null;
-                });
+    private DateTemplate<LocalDate> toLocalDate() {
+        return Expressions.dateTemplate(
+                LocalDate.class,
+                "date({0})",
+                roleHistory.dateTime);
     }
 }
