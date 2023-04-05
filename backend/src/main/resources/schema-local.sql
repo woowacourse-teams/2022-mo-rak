4ca808a23886ba48bf9333d4cb186c5c2e76d2d0
leokim <kunga077@gmail.com>
2023-04-05 18:00:19 +0900

    chore: appointment available time 에서 외래키 제거

--- backend/src/main/resources/schema-local.sql
+++ backend/src/main/resources/schema-local.sql
@@ -132,7 +132,6 @@ CREATE TABLE appointment_available_time
     `created_at`      DATETIME NOT NULL,
     `updated_at`      DATETIME NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (appointment_id) REFERENCES appointment (id) ON DELETE CASCADE,
     FOREIGN KEY (member_id) REFERENCES member (id)
 );
 
