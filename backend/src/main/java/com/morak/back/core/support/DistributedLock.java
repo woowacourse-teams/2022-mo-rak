ac1941eef565d22db0da562a53ad460553708856
leokim <kunga077@gmail.com>
2023-04-06 14:47:37 +0900

    feat: distributed lock 어노테이션과 aspect 구현

---
+++ backend/src/main/java/com/morak/back/core/support/DistributedLock.java
@@ -0,0 +1,18 @@
+package com.morak.back.core.support;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DistributedLock {
+
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+
+    long waitTime() default 10L;
+
+    long leaseTime() default 1L;
+}
---
+++ backend/src/main/java/com/morak/back/core/support/DistributedLockAspect.java
@@ -0,0 +1,54 @@
+package com.morak.back.core.support;
+
+import java.lang.reflect.Method;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Aspect
+@Order(value = 1)
+public class DistributedLockAspect {
+
+    private final RedissonClient redissonClient;
+
+    @Pointcut("@annotation(com.morak.back.core.support.DistributedLock)")
+    private void distributedLockPointCut() {
+
+    }
+
+    @Around("distributedLockPointCut()")
+    public Object lock(final ProceedingJoinPoint joinPoint) throws Throwable {
+        String code = (String) joinPoint.getArgs()[2];
+
+        RLock rLock = redissonClient.getLock(String.format("lock:%s", code));
+
+        DistributedLock lock = getDistributedLock(joinPoint);
+        try {
+            boolean available = rLock.tryLock(lock.waitTime(), lock.leaseTime(), lock.timeUnit());
+            if (!available) {
+                return false;
+            }
+            return joinPoint.proceed();
+        } catch (final Exception e) {
+            Thread.currentThread().interrupt();
+            throw new InterruptedException();
+        } finally {
+            rLock.unlock();
+        }
+    }
+
+    private DistributedLock getDistributedLock(ProceedingJoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        return method.getAnnotation(DistributedLock.class);
+    }
+}
