225eae827735cb92e081642ba44e4ef281e03825
leokim <kunga077@gmail.com>
2023-06-06 23:51:54 +0900

    chore: 의존성을 테스트의존성으로 변경하고 임베디드 레디스 서버 포트 번호 변경

--- backend/src/test/java/com/morak/back/EmbeddedRedisCallback.java
+++ backend/src/test/java/com/morak/back/EmbeddedRedisCallback.java
@@ -7,12 +7,13 @@ import redis.embedded.RedisServer;
 
 public class EmbeddedRedisCallback implements BeforeAllCallback, AfterAllCallback {
 
-    private static final int REDIS_PORT = 6379;
+    private static final int EMBEDDED_REDIS_PORT = 16379;
+
     private RedisServer redisServer;
 
     @Override
     public void beforeAll(ExtensionContext context) {
-        redisServer = new RedisServer(REDIS_PORT);
+        redisServer = new RedisServer(EMBEDDED_REDIS_PORT);
         redisServer.start();
     }
 
