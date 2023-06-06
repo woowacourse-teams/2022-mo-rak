951f564b399301ce3a245cfad130082a44662f7d
leokim <kunga077@gmail.com>
2023-06-06 22:48:51 +0900

    feat: 레디스 관련 콜백 실행되도록 익스텐션 추가

--- backend/src/test/java/com/morak/back/AcceptanceTest.java
+++ backend/src/test/java/com/morak/back/AcceptanceTest.java
@@ -2,12 +2,14 @@ package com.morak.back;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = {"classpath:schema.sql", "classpath:data.sql"})
+@ExtendWith(EmbeddedRedisCallback.class)
 public class AcceptanceTest {
 
     @LocalServerPort
